2a02a76375537a2807abcc242424abd9c8357dab
Corniel <mail@corniel.nl>
2014-05-26 20:52:40 +0200

    Package & assembly info update.

---
+++ src/Shared/ProductInfo.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+// Info about all assemblies.
+[assembly: AssemblyCompany("Qowaiv community")]
+[assembly: AssemblyCopyright("Copyright © Qowaiv community 2013-2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+
+// For optimal .NET support this is enabled in all assemblies.
+[assembly: CLSCompliant(true)]
---
+++ src/Shared/VersionInfo.cs
@@ -0,0 +1,5 @@
+using System.Reflection;
+
+// Version.
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

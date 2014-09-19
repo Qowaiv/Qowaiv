e13c70ac3bbcd9ca648bff1cb14cc168f48e0d46
Corniel <mail@corniel.nl>
2014-09-19 22:18:15 +0200

    Lock move and type.

--- src/Qowaiv/Unknown.cs
+++ src/Qowaiv/Unknown.cs
@@ -8,67 +8,67 @@ using System.Threading.Tasks;
 
 namespace Qowaiv
 {
-    /// <summary>Helps handling the unknown status of Single Value Objects.</summary>
-    /// <remarks>
-    /// The 'unknown' case differes from the 'empy' case. Where 'empty' just means:
-    /// Not set (yet), this is an unset (default) value, 'unknown' means that the user
-    /// has set the value, saying, there must be some value, but I just don't know
-    /// which value it should be.
-    /// 
-    /// Note that not all scenario's that support 'empty' suppport 'unknown' too.
-    /// </remarks>
-    public static class Unknown
-    {
-        /// <summary>Returns true if the string represents unknown, otherwise false.</summary>
-        /// <param name="val">
-        /// The string value to test.
-        /// </param>
-        public static bool IsUnknown(string val)
-        {
-            return IsUnknown(val, CultureInfo.CurrentCulture);
-        }
+	/// <summary>Helps handling the unknown status of Single Value Objects.</summary>
+	/// <remarks>
+	/// The 'unknown' case differes from the 'empty' case. Where 'empty' just means:
+	/// Not set (yet), this is an unset (default) value, 'unknown' means that the user
+	/// has set the value, saying, there must be some value, but I just don't know
+	/// which value it should be.
+	/// 
+	/// Note that not all scenario's that support 'empty' suppport 'unknown' too.
+	/// </remarks>
+	public static class Unknown
+	{
+		/// <summary>Returns true if the string represents unknown, otherwise false.</summary>
+		/// <param name="val">
+		/// The string value to test.
+		/// </param>
+		public static bool IsUnknown(string val)
+		{
+			return IsUnknown(val, CultureInfo.CurrentCulture);
+		}
 
-        /// <summary>Returns true if the string represents unknown, otherwise false.</summary>
-        /// <param name="val">
-        /// The string value to test.
-        /// </param>
-        /// <param name="culture">
-        /// The culture to test for.
-        /// </param>
-        public static bool IsUnknown(string val, CultureInfo culture)
-        {
-            if (string.IsNullOrEmpty(val)) { return false; }
+		/// <summary>Returns true if the string represents unknown, otherwise false.</summary>
+		/// <param name="val">
+		/// The string value to test.
+		/// </param>
+		/// <param name="culture">
+		/// The culture to test for.
+		/// </param>
+		public static bool IsUnknown(string val, CultureInfo culture)
+		{
+			if (string.IsNullOrEmpty(val)) { return false; }
 
-            var c = culture ?? CultureInfo.InvariantCulture;
+			var c = culture ?? CultureInfo.InvariantCulture;
 
-            string[] values;
+			string[] values;
 
-            if (!StringValues.TryGetValue(c, out values))
-            {
-                lock (locker)
-                {
-                    values = ResourceManager.GetString("Values", c).Split(';');
-                    StringValues[c] = values;
-                }
-            }
-            return
-                values.Contains(val.ToLower(c)) ||
-                (
-                    c != CultureInfo.InvariantCulture &&
-                    StringValues[CultureInfo.InvariantCulture].Contains(val.ToLowerInvariant())
-                );
-        }
+			lock (locker)
+			{
+				if (!StringValues.TryGetValue(c, out values))
+				{
+					values = ResourceManager.GetString("Values", c).Split(';');
+					StringValues[c] = values;
+				}
+			}
+			return
+				values.Contains(val.ToLower(c)) ||
+				(
+					c != CultureInfo.InvariantCulture &&
+					StringValues[CultureInfo.InvariantCulture].Contains(val.ToLowerInvariant())
+				);
+		}
 
-        /// <summary>The resource manager managing the culture based string values.</summary>
-        private static Dictionary<CultureInfo, String[]> StringValues = new Dictionary<CultureInfo, String[]>()
+		/// <summary>The resource manager managing the culture based string values.</summary>
+		private static Dictionary<CultureInfo, String[]> StringValues = new Dictionary<CultureInfo, String[]>()
         {
             { CultureInfo.InvariantCulture, new string[]{ "?", "unknown", "not known", "notknown" } },
         };
 
-        /// <summary>The resource manager managing the culture based string values.</summary>
-        private static ResourceManager ResourceManager = new ResourceManager("Qowaiv.UnknownLabels", typeof(Unknown).Assembly);
+		/// <summary>The resource manager managing the culture based string values.</summary>
+		private static ResourceManager ResourceManager = new ResourceManager("Qowaiv.UnknownLabels", typeof(Unknown).Assembly);
 
-        /// <summary>The locker for adding a culture.</summary>
-        private static volatile object locker = new object();
-    }
+		/// <summary>The locker for adding a culture.</summary>
+		private static volatile object locker = new object();
+	}
 }

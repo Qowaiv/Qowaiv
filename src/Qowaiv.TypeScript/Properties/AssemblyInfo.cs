1c8e10b27a219b4e552aed525d546aabe94e6f1d
Corniel <mail@corniel.nl>
2015-05-30 09:32:36 +0200

    Added sigining and assembly info for the Qowaiv.TypeScript assembly.

---
+++ src/Qowaiv.TypeScript/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// Info about the assembly.
+[assembly: AssemblyTitle("Qowaiv.TypeScript")]
+[assembly: AssemblyProduct("Qowaiv.TypeScript")]
+[assembly: AssemblyDescription("Qowaiv Single Value Object library implementations in TypeScript.")]
+
+// No COM visibility.
+[assembly: ComVisible(false)]
+[assembly: Guid("2e418136-3186-4cb2-b90e-e7f5d7c56e87")]

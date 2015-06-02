ae5b5eec433143ce024b042b6a5ba9a9f4b6f650
Corniel <mail@corniel.nl>
2015-06-02 20:14:42 +0200

    CountryToCurrency files (missed at the previous commit).

---
+++ src/Qowaiv/CountryToCurrency.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qowaiv
+{
+	internal partial struct CountryToCurrency
+	{
+		private Country m_Country;
+		private Currency m_Currency;
+		private Date m_StartDate;
+		
+
+		public CountryToCurrency(Country country, Currency currency, Date startdate)
+		{
+			m_Country = country;
+			m_Currency = currency;
+			m_StartDate = startdate;
+		}
+
+		public CountryToCurrency(Country country, Currency currency) 
+			: this(country, currency, Date.MinValue) { }
+
+		public Country Country { get { return m_Country; } }
+		public Currency Currency { get { return m_Currency; } }
+		public Date StartDate { get { return m_StartDate; } }
+	}
+}

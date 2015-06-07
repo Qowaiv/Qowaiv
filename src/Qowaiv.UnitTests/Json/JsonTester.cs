582fab31435182a13e5ff883111fc1ba71699e57
Corniel <mail@corniel.nl>
2015-06-07 20:36:56 +0200

    Added support for reference types to JsonTester.

--- src/Qowaiv.UnitTests/Json/JsonTester.cs
+++ src/Qowaiv.UnitTests/Json/JsonTester.cs
@@ -1,47 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Qowaiv.Json;
+﻿using Qowaiv.Json;
+using System;
 
 namespace Qowaiv.UnitTests.Json
 {
-    public class JsonTester
-    {
-        public static T Read<T>() where T : IJsonSerializable
-        {
-            T instance = default(T);
-            instance.FromJson();
-            return instance;
-        }
-        public static T Read<T>(String val) where T : IJsonSerializable
-        {
-            T instance = default(T);
-            instance.FromJson(val);
-            return instance;
-        }
-        public static T Read<T>(Int64 val) where T : IJsonSerializable
-        {
-            T instance = default(T);
-            instance.FromJson(val);
-            return instance;
-        }
-        public static T Read<T>(Double val) where T : IJsonSerializable
-        {
-            T instance = default(T);
-            instance.FromJson(val);
-            return instance;
-        }
-        public static T Read<T>(DateTime val) where T : IJsonSerializable
-        {
-            T instance = default(T);
-            instance.FromJson(val);
-            return instance;
-        }
+	public class JsonTester
+	{
+		public static T Read<T>() where T : IJsonSerializable
+		{
+			T instance = default(T);
+			if(typeof(T).IsValueType)
+			{
+				instance.FromJson();
+			}
+			return instance;
+		}
+		public static T Read<T>(String val) where T : IJsonSerializable
+		{
+			T instance = NewInstance<T>();
+			instance.FromJson(val);
+			return instance;
+		}
+		public static T Read<T>(Int64 val) where T : IJsonSerializable
+		{
+			T instance = NewInstance<T>();
+			instance.FromJson(val);
+			return instance;
+		}
+		public static T Read<T>(Double val) where T : IJsonSerializable
+		{
+			T instance = NewInstance<T>();
+			instance.FromJson(val);
+			return instance;
+		}
+		public static T Read<T>(DateTime val) where T : IJsonSerializable
+		{
+			T instance = NewInstance<T>();
+			instance.FromJson(val);
+			return instance;
+		}
 
-        public static object Write(IJsonSerializable val)
-        {
-            return val.ToJson();
-        }
-    }
+		private static T NewInstance<T>() where T : IJsonSerializable
+		{
+			return typeof(T).IsValueType ? default(T) : Activator.CreateInstance<T>();
+		}
+
+		public static object Write(IJsonSerializable val)
+		{
+			return val.ToJson();
+		}
+	}
 }

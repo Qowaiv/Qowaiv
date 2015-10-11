bc2149bb6a51fb8e862d371fe12f3f1af5a2e0c6
Corniel <mail@corniel.nl>
2015-10-11 14:22:36 +0200

    First attempt of creating a NuGet package.

--- src/Shared/VersionInfo.cs
+++ src/Shared/VersionInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
 // Version.
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.138")]
+[assembly: AssemblyFileVersion("1.0.0.138")]

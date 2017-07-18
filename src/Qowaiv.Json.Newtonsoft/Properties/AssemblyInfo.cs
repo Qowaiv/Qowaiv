e98bca4002a25cd80e1437958447b9eecae866f2
Vincent Lesierse <vincent@lesierse.com>
2017-07-18 14:17:00 +0200

    Change projects to support .NET Standard 2.0

--- src/Qowaiv.Json.Newtonsoft/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-
-// Info about the assembly.
-[assembly: AssemblyTitle("Qowaiv.Json.Newtonsoft")]
-[assembly: AssemblyProduct("Qowaiv.Json.Newtonsoft")]
-[assembly: AssemblyDescription("Qowaiv Single Value Object library extensions for JSON with Newtonsoft.")]

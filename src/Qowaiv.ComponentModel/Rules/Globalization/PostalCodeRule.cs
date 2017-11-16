c3c7695e376ee49edcf5c15e1b7f220eda9bc37d
Corniel <corniel@gmail.com>
2017-11-16 09:17:08 +0100

    Tabs => spaces.

--- src/Qowaiv.ComponentModel/Rules/Globalization/PostalCodeRule.cs
+++ src/Qowaiv.ComponentModel/Rules/Globalization/PostalCodeRule.cs
@@ -5,11 +5,11 @@ using System;
 
 namespace Qowaiv.ComponentModel.Rules.Globalization
 {
-	/// <summary>Rule that can validate a <see cref="Qowaiv.PostalCode"/> 
-	/// being valid for a specific <see cref="Qowaiv.Globalization.Country"/>.
-	/// </summary>
-	public class PostalCodeRule : ValidationRule
-	{
+    /// <summary>Rule that can validate a <see cref="Qowaiv.PostalCode"/> 
+    /// being valid for a specific <see cref="Qowaiv.Globalization.Country"/>.
+    /// </summary>
+    public class PostalCodeRule : ValidationRule
+    {
         /// <summary>Creates a new instance of a <see cref="PostalCodeRule"/>.</summary>
         /// <param name="postalCode">
         /// The postal code to validate.
@@ -25,15 +25,15 @@ namespace Qowaiv.ComponentModel.Rules.Globalization
         /// </param>
         public PostalCodeRule(PostalCode postalCode, Country country, string postalCodeName, string countryName)
             : base(postalCodeName, countryName)
-		{
-			PostalCode = postalCode;
-			Country = country;
-		}
+        {
+            PostalCode = postalCode;
+            Country = country;
+        }
 
-		/// <summary>Gets the involved postal code.</summary>
-		public PostalCode PostalCode { get; }
-		/// <summary>Gets the involved country.</summary>
-		public Country Country { get;  }
+        /// <summary>Gets the involved postal code.</summary>
+        public PostalCode PostalCode { get; }
+        /// <summary>Gets the involved country.</summary>
+        public Country Country { get; }
 
         /// <summary>Returns true if postal code is valid (or if the postal code or country is empty or unknown).</summary>
         protected override bool IsValid(ValidationContext validationContext)
@@ -42,15 +42,15 @@ namespace Qowaiv.ComponentModel.Rules.Globalization
         }
 
         /// <summary>Gets the error message.</summary>
-        protected override Func<string> ErrorMessageString => ()=> string.Format(QowaivComponentModelMessages.PostalCodeValidator_ErrorMessage, PostalCode, Country.DisplayName);
+        protected override Func<string> ErrorMessageString => () => string.Format(QowaivComponentModelMessages.PostalCodeValidator_ErrorMessage, PostalCode, Country.DisplayName);
 
-		/// <summary>Returns <see cref="ValidationMessage.None"/> if the <see cref="PostalCode"/> 
-		/// is valid for a specific <see cref="Country"/>, otherwise false.
-		/// </summary>
-		public static ValidationResult IsValid(PostalCode postalCode, Country country, string postalCodeName, string countryName)
-		{
-			var rule = new PostalCodeRule(postalCode, country, postalCodeName, countryName);
-			return rule.Validate(null);
-		}
-	}
+        /// <summary>Returns <see cref="ValidationMessage.None"/> if the <see cref="PostalCode"/> 
+        /// is valid for a specific <see cref="Country"/>, otherwise false.
+        /// </summary>
+        public static ValidationResult IsValid(PostalCode postalCode, Country country, string postalCodeName, string countryName)
+        {
+            var rule = new PostalCodeRule(postalCode, country, postalCodeName, countryName);
+            return rule.Validate(null);
+        }
+    }
 }
--- src/Qowaiv.ComponentModel/Rules/ValidationRule.cs
+++ src/Qowaiv.ComponentModel/Rules/ValidationRule.cs
@@ -12,7 +12,7 @@ namespace Qowaiv.ComponentModel.Rules
     /// during <see cref="IValidatableObject.Validate(ValidationContext)"/>.
     /// </summary>
     public abstract class ValidationRule : IValidationRule
-	{
+    {
         /// <summary>Creates a new instance of a <see cref="ValidationRule"/>.</summary>
         /// <param name="propertyNames">
         /// The involved properties.
@@ -32,16 +32,16 @@ namespace Qowaiv.ComponentModel.Rules
         /// <summary>Gets the involved property names.</summary>
         public string[] PropertyNames { get; }
 
-		/// <summary>Gets and set an error message to if validation fails.</summary>
-		public string ErrorMessage { get; set; }
+        /// <summary>Gets and set an error message to if validation fails.</summary>
+        public string ErrorMessage { get; set; }
 
-		/// <summary>Gets and set the error message resource name to use in order to look up the System.ComponentModel.DataAnnotations.ValidationAttribute.ErrorMessageResourceType
-		/// property value if validation fails.
-		/// </summary>
-		public string ErrorMessageResourceName { get; set; }
+        /// <summary>Gets and set the error message resource name to use in order to look up the System.ComponentModel.DataAnnotations.ValidationAttribute.ErrorMessageResourceType
+        /// property value if validation fails.
+        /// </summary>
+        public string ErrorMessageResourceName { get; set; }
 
-		/// <summary>Gets or sets the resource type to use for error-message lookup if validation fails.</summary>
-		public Type ErrorMessageResourceType { get; set; }
+        /// <summary>Gets or sets the resource type to use for error-message lookup if validation fails.</summary>
+        public Type ErrorMessageResourceType { get; set; }
 
         /// <summary>Gets the localized validation error message.</summary>
         protected virtual Func<string> ErrorMessageString => GetResourceErrorMessage() ?? (() => ErrorMessage);
@@ -77,7 +77,7 @@ namespace Qowaiv.ComponentModel.Rules
                             ErrorMessageResourceType.FullName,
                             property.Name));
                     }
-                    return ()=> (string)property.GetValue(null, null);
+                    return () => (string)property.GetValue(null, null);
                 }
             }
             throw new InvalidOperationException(
@@ -88,49 +88,49 @@ namespace Qowaiv.ComponentModel.Rules
                 ErrorMessageResourceName));
         }
 
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <param name="validationContext"></param>
-		/// <returns></returns>
-		public virtual ValidationResult Validate(ValidationContext validationContext)
-		{
-			if(IsValid(validationContext))
-			{
-				return ValidationMessage.None;
-			}
-			return ValidationMessage.Error(string.Format(CultureInfo.CurrentCulture, ErrorMessageString(), PropertyNames), PropertyNames);
-		}
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="validationContext"></param>
+        /// <returns></returns>
+        public virtual ValidationResult Validate(ValidationContext validationContext)
+        {
+            if (IsValid(validationContext))
+            {
+                return ValidationMessage.None;
+            }
+            return ValidationMessage.Error(string.Format(CultureInfo.CurrentCulture, ErrorMessageString(), PropertyNames), PropertyNames);
+        }
 
-		/// <Summary>Determines whether the specified value of the object is valid.</Summary>
-		/// <param name="validationContext">
-		/// </param>
-		/// <returns>
-		/// <code>true</code> if the specified value is valid, otherwise <code>false</code>.
-		/// </returns>
-		protected abstract bool IsValid(ValidationContext validationContext);
+        /// <Summary>Determines whether the specified value of the object is valid.</Summary>
+        /// <param name="validationContext">
+        /// </param>
+        /// <returns>
+        /// <code>true</code> if the specified value is valid, otherwise <code>false</code>.
+        /// </returns>
+        protected abstract bool IsValid(ValidationContext validationContext);
 
-		/// <summary>Creates an <see cref="IValidationRule"/> based on the expression.</summary>
-		/// <param name="isValid">
-		/// The isValid expression.
-		/// </param>
-		/// <param name="message">
-		/// The message to show when the rule is not valid.
-		/// </param>
-		/// <param name="propertyNames">
-		/// The involved property names.
-		/// </param>
-		/// <remarks>
-		/// If the <code>bool?</code> has no value, the rule is assumed not to be invalid.
-		/// This helps in scenario's like these:
-		/// <code>
-		/// ValidationRule.For((context) => context.GetService&lt;SomeService&gt;()?.IsValid(), "Error");
-		/// </code>
-		/// If The service could not be resolved, the rule will be ignored.
-		/// </remarks>
-		public static IValidationRule For(Func<ValidationContext, bool?> isValid, Func<string> message, params string[] propertyNames)
-		{
-			return new ValidationExpression(isValid, message, propertyNames);
-		}
-	}
+        /// <summary>Creates an <see cref="IValidationRule"/> based on the expression.</summary>
+        /// <param name="isValid">
+        /// The isValid expression.
+        /// </param>
+        /// <param name="message">
+        /// The message to show when the rule is not valid.
+        /// </param>
+        /// <param name="propertyNames">
+        /// The involved property names.
+        /// </param>
+        /// <remarks>
+        /// If the <code>bool?</code> has no value, the rule is assumed not to be invalid.
+        /// This helps in scenario's like these:
+        /// <code>
+        /// ValidationRule.For((context) => context.GetService&lt;SomeService&gt;()?.IsValid(), "Error");
+        /// </code>
+        /// If The service could not be resolved, the rule will be ignored.
+        /// </remarks>
+        public static IValidationRule For(Func<ValidationContext, bool?> isValid, Func<string> message, params string[] propertyNames)
+        {
+            return new ValidationExpression(isValid, message, propertyNames);
+        }
+    }
 }

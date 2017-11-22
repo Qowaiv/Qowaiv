cff73e1df785b616aaa51254f163da52550c8980
Corniel <corniel@gmail.com>
2017-11-22 22:36:24 +0100

    Old project format.

---
+++ test/Qowaiv.CodeGenerator.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+
+// Info about the assembly.
+[assembly: AssemblyTitle("Qowaiv.CodeGeration.UnitTests")]
+[assembly: AssemblyProduct("Qowaiv.CodeGeration.UnitTests")]
+[assembly: AssemblyDescription("Qowaiv Single Value Object library unit tests")]

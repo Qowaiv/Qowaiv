57fd4548d9b8ba9436d5cc0fae756fefe16fe13c
Corniel <mail@corniel.nl>
2018-04-10 19:14:34 +0200

    Extend TypeConverter for Date.

--- src/Qowaiv/Conversion/DateTypeConverter.cs
+++ src/Qowaiv/Conversion/DateTypeConverter.cs
@@ -4,84 +4,101 @@ using System.Globalization;
 
 namespace Qowaiv.Conversion
 {
-	/// <summary>Provides a conversion for a Date.</summary>
-	public class DateTypeConverter : TypeConverter
-	{
-		#region Convert From
+    /// <summary>Provides a conversion for a Date.</summary>
+    public class DateTypeConverter : TypeConverter
+    {
+        #region Convert From
 
-		/// <summary>Returns whether this converter can convert an string to
-		/// a Date, using the specified context.
-		/// </summary>
-		/// <param name="context">
-		/// An System.ComponentModel.ITypeDescriptorContext that provides a format context.
-		/// </param>
-		/// <param name="sourceType">
-		/// A System.Type that represents the type you want to convert from.
-		/// </param>
-		/// <returns>
-		/// true if this converter can perform the conversion; otherwise, false.
-		/// </returns>
-		public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
-		{
-			return sourceType == typeof(string) || base.CanConvertFrom(context, sourceType);
-		}
-		/// <summary>Converts a string to a Date, using the specified
-		/// context and culture information.
-		/// </summary>
-		/// <param name="context">
-		/// An System.ComponentModel.ITypeDescriptorContext that provides a format context.
-		/// </param>
-		/// <param name="culture">
-		/// The System.Globalization.CultureInfo to use as the current culture.
-		/// </param>
-		/// <param name="value">
-		/// The System.Object to convert.
-		/// </param>
-		/// <returns>
-		/// An System.Object that represents the converted value.
-		/// </returns>
-		/// <exception cref="NotSupportedException">
-		/// The conversion cannot be performed.
-		/// </exception>
-		public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
-		{
-			var str = value as string;
-			if (str != null)
-			{
-				return Date.Parse(str, culture);
-			}
-			return base.ConvertFrom(context, culture, value);
-		}
-		#endregion
+        /// <summary>Returns whether this converter can convert an string to
+        /// a Date, using the specified context.
+        /// </summary>
+        /// <param name="context">
+        /// An System.ComponentModel.ITypeDescriptorContext that provides a format context.
+        /// </param>
+        /// <param name="sourceType">
+        /// A System.Type that represents the type you want to convert from.
+        /// </param>
+        /// <returns>
+        /// true if this converter can perform the conversion; otherwise, false.
+        /// </returns>
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            return
+                sourceType == typeof(string) ||
+                sourceType == typeof(DateTime) ||
+                base.CanConvertFrom(context, sourceType);
+        }
+        /// <summary>Converts a string to a Date, using the specified
+        /// context and culture information.
+        /// </summary>
+        /// <param name="context">
+        /// An System.ComponentModel.ITypeDescriptorContext that provides a format context.
+        /// </param>
+        /// <param name="culture">
+        /// The System.Globalization.CultureInfo to use as the current culture.
+        /// </param>
+        /// <param name="value">
+        /// The System.Object to convert.
+        /// </param>
+        /// <returns>
+        /// An System.Object that represents the converted value.
+        /// </returns>
+        /// <exception cref="NotSupportedException">
+        /// The conversion cannot be performed.
+        /// </exception>
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            if (value is string str)
+            {
+                return Date.Parse(str, culture);
+            }
+            if(value is DateTime dt)
+            {
+                return (Date)dt;
+            }
+            return base.ConvertFrom(context, culture, value);
+        }
+        #endregion
 
-		#region Convert To
+        #region Convert To
 
-		/// <summary>Converts a Date to string, using the specified context and culture information.</summary>
-		/// <param name="culture">
-		/// A System.Globalization.CultureInfo. If null is passed, the current culture is assumed.
-		/// </param>
-		/// <param name="context">
-		/// An System.ComponentModel.ITypeDescriptorContext that provides a format context.
-		/// </param>
-		/// <param name="value">
-		/// The Date to convert.
-		/// </param>
-		/// <param name="destinationType">
-		/// The System.Type to convert the value parameter to.
-		/// </param>
-		/// <returns>
-		/// A string that represents the converted Date.
-		/// </returns>
-		/// <exception cref="ArgumentNullException">
-		/// The destinationType parameter is null.
-		/// </exception>
-		/// <exception cref="NotSupportedException">
-		/// The conversion cannot be performed.
-		/// </exception>
-		public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
-		{
-			return base.ConvertTo(context, culture, value, destinationType);
-		}
-		#endregion
-	}
+        /// <inheritdoc />
+        public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
+        {
+            return destinationType == typeof(DateTime) || base.CanConvertTo(context, destinationType);
+        }
+
+        /// <summary>Converts a Date to string, using the specified context and culture information.</summary>
+        /// <param name="culture">
+        /// A System.Globalization.CultureInfo. If null is passed, the current culture is assumed.
+        /// </param>
+        /// <param name="context">
+        /// An System.ComponentModel.ITypeDescriptorContext that provides a format context.
+        /// </param>
+        /// <param name="value">
+        /// The Date to convert.
+        /// </param>
+        /// <param name="destinationType">
+        /// The System.Type to convert the value parameter to.
+        /// </param>
+        /// <returns>
+        /// A string that represents the converted Date.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// The destinationType parameter is null.
+        /// </exception>
+        /// <exception cref="NotSupportedException">
+        /// The conversion cannot be performed.
+        /// </exception>
+        public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+        {
+            if(destinationType == typeof(DateTime))
+            {
+                var typed = Guard.IsTypeOf<Date>(value, nameof(value));
+                return (DateTime)typed;
+            }
+            return base.ConvertTo(context, culture, value, destinationType);
+        }
+        #endregion
+    }
 }

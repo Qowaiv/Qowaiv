4eb541b20f17d46a4701b1e66bffb45b9eec8bd3
Corniel Nobel <mail@corniel.nl>
2019-03-28 22:34:24 +0100

    MandatoryAttribute should by default not be valid for unknown values (#40)
     Qowaiv.ComponentModel.DataAnnotations.MandatoryAttribute should by default not be valid for unknown values.

--- src/Qowaiv.ComponentModel/DataAnnotations/MandatoryAttribute.cs
+++ src/Qowaiv.ComponentModel/DataAnnotations/MandatoryAttribute.cs
@@ -7,14 +7,29 @@ namespace Qowaiv.ComponentModel.DataAnnotations
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
     public sealed class MandatoryAttribute : RequiredAttribute
     {
+        /// <summary>Gets or sets a value that indicates whether an empty string is allowed.</summary>
+        public bool AllowUnknownValue { get; set; }
+
         /// <summary>Returns true if the value is not null and value types are
         /// not equal to their default value, otherwise false.
         /// </summary>
+        /// <remarks>
+        /// The unknown value is expected to be static field or property of the type with the name "Unknown".
+        /// </remarks>
         public override bool IsValid(object value)
         {
-            if (value != null && value.GetType().IsValueType)
+            if (value != null)
             {
-                return !value.Equals(Activator.CreateInstance(value.GetType()));
+                var type = value.GetType();
+
+                if (!AllowUnknownValue && value.Equals(Unknown.Value(type)))
+                {
+                    return false;
+                }
+                if (type.IsValueType)
+                {
+                    return !value.Equals(Activator.CreateInstance(value.GetType()));
+                }
             }
             return base.IsValid(value);
         }

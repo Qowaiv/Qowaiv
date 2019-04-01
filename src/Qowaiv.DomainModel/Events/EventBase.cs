bf18348a5700c5a996b9cf2b8f7e56434c57a53f
Corniel Nobel <mail@corniel.nl>
2019-04-01 11:06:14 +0200

    Extended event debug experience.

--- src/Qowaiv.DomainModel/Events/EventBase.cs
+++ src/Qowaiv.DomainModel/Events/EventBase.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
 
 namespace Qowaiv.DomainModel.Events
 {
     /// <summary>A basic implementation of an <see cref="IEvent"/>.</summary>
+    [DebuggerDisplay("{DebuggerDisplay}")]
     public abstract class EventBase : IEvent
     {
         /// <inheritdoc />
@@ -10,5 +15,41 @@ namespace Qowaiv.DomainModel.Events
 
         /// <inheritdoc />
         public virtual int Version { get; set; }
+
+        /// <summary>Represents the event as a DEBUG <see cref="string"/>.</summary>
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        protected virtual string DebuggerDisplay
+        {
+            get
+            {
+                var props = GetType()
+                 .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                 .Where(p => p.Name != nameof(Id) && p.Name != nameof(Version))
+                 .ToArray();
+
+                // Get rid of Event suffix.
+                var eventName = GetType().Name;
+                if (eventName.EndsWith("Event")) { eventName = eventName.Substring(eventName.Length - 5); }
+
+                var sb = new StringBuilder()
+                    .AppendFormat("v{0} ", Version)
+                    .AppendFormat(eventName)
+                    .AppendFormat(", Props[{0}] {{ ", props.Length);
+
+                for(var i = 0; i < props.Length; i++)
+                {
+                    var prop = props[i];
+                    if(i != 0)
+                    {
+                        sb.Append(", ");
+                    }
+                    sb.AppendFormat("{0}: {1}", prop.Name, prop.GetValue(this));
+
+                }
+
+                sb.Append(" }, Id: ").Append(Id.ToString().ToUpperInvariant());
+                return sb.ToString();
+            }
+        }
     }
 }

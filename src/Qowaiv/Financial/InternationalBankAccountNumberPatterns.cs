e003046cbf1c537ab6ccd107e769cb434fc4fe0e
Corniel Nobel <mail@corniel.nl>
2019-07-25 15:42:15 +0200

    Added IBAN support for some countries (#65)
    Official IBAN countries:
    * BY (Belarus)
    * TL (Timor Leste)
    * VA (Vatican City)
    
    Candidate countries:
    * KM (Comoros)
    * TD (Chad)
    * EG (Egypt)
    * GA (Gabon)
    * HN (Honduras)
    * MA (Morocco)
    * NI (Nicaragua)
    * NE (Niger)
    * TG (Togo)

--- src/Qowaiv/Financial/InternationalBankAccountNumberPatterns.cs
+++ src/Qowaiv/Financial/InternationalBankAccountNumberPatterns.cs
@@ -15,18 +15,12 @@ namespace Qowaiv.Financial
         /// See http://en.wikipedia.org/wiki/International_Bank_Account_Number.
         /// </remarks>
         private static readonly Dictionary<Country, Regex> LocalizedPatterns = new Dictionary<Country, Regex>()
-        {
+        {   
             // Albania, Length: 28, BBAN: 8n, 16c, Fields: ALkk bbbs sssx cccc cccc cccc cccc (b = National bank code, s = Branch code, x = National check digit, c = Account number)
             { Country.AL, new Regex(@"^AL[0-9]{10}[0-9A-Z]{16}$", RegexOptions.Compiled) },
-
-            // Algeria, Length: 24, BBAN: 20n, Fields: DZkk nnnn nnnn nnnn nnnn nnnn (Nordea catalogued)
-            { Country.DZ, new Regex(@"^DZ[0-9]{22}$", RegexOptions.Compiled) },
-
+        
             // Andorra, Length: 24, BBAN: 8n,12c, Fields: ADkk bbbb ssss cccc cccc cccc (b = National bank code, s = Branch code, c = Account number)
             { Country.AD, new Regex(@"^AD[0-9]{10}[0-9A-Z]{12}$", RegexOptions.Compiled) },
-
-            // Angola, Length: 25, BBAN: 21n, Fields: AOkk nnnn nnnn nnnn nnnn nnnn n (Nordea catalogued)
-            { Country.AO, new Regex(@"^AO[0-9]{23}$", RegexOptions.Compiled) },
 
             // Austria, Length: 20, BBAN: 16n, Fields: ATkk bbbb bccc cccc cccc (b = National bank code, c = Account number)
             { Country.AT, new Regex(@"^AT[0-9]{18}$", RegexOptions.Compiled) },
@@ -37,12 +31,12 @@ namespace Qowaiv.Financial
             // Bahrain, Length: 22, BBAN: 4a,14c, Fields: BHkk bbbb cccc cccc cccc cc (b = National bank code, c = Account number)
             { Country.BH, new Regex(@"^BH[0-9]{2}[A-Z]{4}[0-9A-Z]{14}$", RegexOptions.Compiled) },
 
+            // Belarus, Length: 28, BBAN: 4c, 20n, Fields: BYkk bbbb aaaa cccc cccc cccc cccc (b = National bank code, a = Balance account number, c = Account number)
+            { Country.BY, new Regex(@"^BY[0-9]{2}[A-Z0-9]{24}$", RegexOptions.Compiled) },
+
             // Belgium, Length: 16, BBAN: 12n, Fields: BEkk bbbc cccc ccxx (b = National bank code, c = Account number, x = National check digits)
             { Country.BE, new Regex(@"^BE[0-9]{14}$", RegexOptions.Compiled) },
-
-            // Benin, Length: 28, BBAN: 1a, 23n, Fields: BJkk annn nnnn nnnn nnnn nnnn nnnn (Nordea catalogued)
-            { Country.BJ, new Regex(@"^BJ[0-9]{2}[A-Z][0-9]{23}$", RegexOptions.Compiled) },
-
+       
             // Bosnia and Herzegovina, Length: 20, BBAN: 16n, Fields: BAkk bbbs sscc cccc ccxx (k = IBAN check digits (always 39), b = National bank code, s = Branch code, c = Account number, x = National check digits)
             { Country.BA, new Regex(@"^BA39[0-9]{16}$", RegexOptions.Compiled) },
 
@@ -51,18 +45,6 @@ namespace Qowaiv.Financial
 
             // Bulgaria, Length: 22, BBAN: 4a,6n,8c, Fields: BGkk bbbb ssss ddcc cccc cc (b = BIC bank code, s = Branch (BAE) number, d = Account type, c = Account number)
             { Country.BG, new Regex(@"^BG[0-9]{2}[A-Z]{4}[0-9]{6}[0-9A-Z]{8}$", RegexOptions.Compiled) },
-
-            // Burkina Faso, Length: 27, BBAN: 23n, Fields: BFkk nnnn nnnn nnnn nnnn nnnn nnn (Nordea catalogued)
-            { Country.BF, new Regex(@"^BF[0-9]{25}$", RegexOptions.Compiled) },
-
-            // Burundi, Length: 16, BBAN: 12n, Fields: BIkk nnnn nnnn nnnn (Nordea catalogued)
-            { Country.BI, new Regex(@"^BI[0-9]{14}$", RegexOptions.Compiled) },
-
-            // Cameroon, Length: 27, BBAN: 23n, Fields: CMkk nnnn nnnn nnnn nnnn nnnn nnn (Nordea catalogued)
-            { Country.CM, new Regex(@"^CM[0-9]{25}$", RegexOptions.Compiled) },
-
-            // Cape Verde, Length: 25, BBAN: 21n, Fields: CVkk nnnn nnnn nnnn nnnn nnnn n (Nordea catalogued)
-            { Country.CV, new Regex(@"^CV[0-9]{23}$", RegexOptions.Compiled) },
 
             // Costa Rica, Length: 21, BBAN: 17n, Fields: CRkk bbbc cccc cccc cccc c (b = bank code, c = Account number)
             { Country.CR, new Regex(@"^CR[0-9]{19}$", RegexOptions.Compiled) },
@@ -81,6 +63,9 @@ namespace Qowaiv.Financial
 
             // Dominican Republic, Length: 28, BBAN: 4a,20n, Fields: DOkk bbbb cccc cccc cccc cccc cccc (b = Bank identifier, c = Account number)
             { Country.DO, new Regex(@"^DO[0-9]{2}[A-Z]{4}[0-9]{20}$", RegexOptions.Compiled) },
+
+            // East Timor, Length: 23, BBAN: 2n, 17c 2n, Fields: TLkk bbbc cccc cccc cccc cxx (k = IBAN check digits (always = "38"), b = Bank identifier, c = Account number, x = National check digits)
+            { Country.TL, new Regex(@"^TL38[0-9A-Z]{17}[0-9]{2}$", RegexOptions.Compiled) },
 
             // Estonia, Length: 20, BBAN: 16n, Fields: EEkk bbss cccc cccc cccx (b = National bank code, s = Branch code, c = Account number, x = National check digit)
             { Country.EE, new Regex(@"^EE[0-9]{18}$", RegexOptions.Compiled) },
@@ -118,9 +103,6 @@ namespace Qowaiv.Financial
             // Iceland, Length: 26, BBAN: 22n, Fields: ISkk bbbb sscc cccc iiii iiii ii (b = National bank code, s = Branch code, c = Account number, i = holder's kennitala (national identification number).)
             { Country.IS, new Regex(@"^IS[0-9]{24}$", RegexOptions.Compiled) },
 
-            // Iran, Length: 26, BBAN: 22n, Fields: IRkk nnnn nnnn nnnn nnnn nnnn nn (Nordea catalogued)
-            { Country.IR, new Regex(@"^IR[0-9]{24}$", RegexOptions.Compiled) },
-
             // Ireland, Length: 22, BBAN: 4c,14n, Fields: IEkk aaaa bbbb bbcc cccc cc (a = BIC bank code, b = Bank/branch code (sort code), c = Account number)
             { Country.IE, new Regex(@"^IE[0-9]{2}[0-9A-Z]{4}[0-9]{14}$", RegexOptions.Compiled) },
 
@@ -129,9 +111,6 @@ namespace Qowaiv.Financial
 
             // Italy, Length: 27, BBAN: 1a,10n,12c, Fields: ITkk xaaa aabb bbbc cccc cccc ccc (x = Check char (CIN), a = National bank code (it:Associazione bancaria italiana or Codice ABI ), b = Branch code (it:Coordinate bancarie or CAB– Codice d'Avviamento Bancario), c = Account number)
             { Country.IT, new Regex(@"^IT[0-9]{2}[A-Z][0-9]{10}[0-9A-Z]{12}$", RegexOptions.Compiled) },
-
-            // Ivory Coast, Length: 28, BBAN: 1a, 23n, Fields: CIkk annn nnnn nnnn nnnn nnnn nnnn (Nordea catalogued)
-            { Country.CI, new Regex(@"^CI[0-9]{2}[A-Z][0-9]{23}$", RegexOptions.Compiled) },
 
             // Jordan, Length: 30, BBAN: 4a, 22n, Fields: JOkk bbbb nnnn 0000 0000 cccc cccc cc (b = National bank, n = Branch code, c = Account number)
             { Country.JO, new Regex(@"^JO[0-9]{2}[A-Z]{4}[0-9]{22}$", RegexOptions.Compiled) },
@@ -160,15 +139,6 @@ namespace Qowaiv.Financial
             // Luxembourg, Length: 20, BBAN: 3n,13c, Fields: LUkk bbbc cccc cccc cccc (b = National bank code, c = Account number)
             { Country.LU, new Regex(@"^LU[0-9]{5}[0-9A-Z]{13}$", RegexOptions.Compiled) },
 
-            // Macedonia, Length: 19, BBAN: 3n,10c,2n, Fields: MKkk bbbc cccc cccc cxx (k = IBAN check digits (always = "07"), b = National bank code, c = Account number, x = National check digits)
-            { Country.MK, new Regex(@"^MK07[0-9]{3}[0-9A-Z]{10}[0-9][0-9]$", RegexOptions.Compiled) },
-
-            // Madagascar, Length: 27, BBAN: 23n, Fields: MGkk nnnn nnnn nnnn nnnn nnnn nnn (Nordea catalogued)
-            { Country.MG, new Regex(@"^MG[0-9]{25}$", RegexOptions.Compiled) },
-
-            // Mali, Length: 28, BBAN: 1a, 23n, Fields: MLkk annn nnnn nnnn nnnn nnnn nnnn (Nordea catalogued)
-            { Country.ML, new Regex(@"^ML[0-9]{2}[A-Z][0-9]{23}$", RegexOptions.Compiled) },
-
             // Malta, Length: 31, BBAN: 4a,5n,18c, Fields: MTkk bbbb ssss sccc cccc cccc cccc ccc (b = BIC bank code, s = Branch code, c = Account number)
             { Country.MT, new Regex(@"^MT[0-9]{2}[A-Z]{4}[0-9]{5}[0-9A-Z]{18}$", RegexOptions.Compiled) },
 
@@ -187,11 +157,11 @@ namespace Qowaiv.Financial
             // Montenegro, Length: 22, BBAN: 18n, Fields: MEkk bbbc cccc cccc cccc xx (k = IBAN check digits (always = "25"), b = Bank code, c = Account number, x = National check digits)
             { Country.ME, new Regex(@"^ME25[0-9]{18}$", RegexOptions.Compiled) },
 
-            // Mozambique, Length: 25, BBAN: 21n, Fields: MZkk nnnn nnnn nnnn nnnn nnnn n (Nordea catalogued)
-            { Country.MZ, new Regex(@"^MZ[0-9]{23}$", RegexOptions.Compiled) },
-
             // Netherlands, Length: 18, BBAN: 4a,10n, Fields: NLkk bbbb cccc cccc cc (b = BIC Bank code, c = Account number)
             { Country.NL, new Regex(@"^NL[0-9]{2}[A-Z]{4}[0-9]{10}$", RegexOptions.Compiled) },
+            
+            // North Macedonia, Length: 19, BBAN: 3n,10c,2n, Fields: MKkk bbbc cccc cccc cxx (k = IBAN check digits (always = "07"), b = National bank code, c = Account number, x = National check digits)
+            { Country.MK, new Regex(@"^MK07[0-9]{3}[0-9A-Z]{10}[0-9][0-9]$", RegexOptions.Compiled) },
 
             // Norway, Length: 15, BBAN: 11n, Fields: NOkk bbbb cccc ccx (b = National bank code, c = Account number, x = Modulo-11 national check digit)
             { Country.NO, new Regex(@"^NO[0-9]{13}$", RegexOptions.Compiled) },
@@ -199,7 +169,7 @@ namespace Qowaiv.Financial
             // Pakistan, Length: 24, BBAN: 4c,16n, Fields: PKkk bbbb cccc cccc cccc cccc (b = National bank code, c = Account number)
             { Country.PK, new Regex(@"^PK[0-9]{2}[0-9A-Z]{4}[0-9]{16}$", RegexOptions.Compiled) },
 
-            // Palestinian, Length: 29, BBAN: 4c,21n, Fields: PSkk bbbb xxxx xxxx xccc cccc cccc c (b = National bank code, c = Account number, x = Not specified)
+            // Palestinian territories, Length: 29, BBAN: 4c,21n, Fields: PSkk bbbb xxxx xxxx xccc cccc cccc c (b = National bank code, c = Account number, x = Not specified)
             { Country.PS, new Regex(@"^PS[0-9]{2}[0-9A-Z]{4}[0-9]{21}$", RegexOptions.Compiled) },
 
             // Poland, Length: 28, BBAN: 24n, Fields: PLkk bbbs sssx cccc cccc cccc cccc (b = National bank code, s = Branch code, x = National check digit, c = Account number)
@@ -219,9 +189,6 @@ namespace Qowaiv.Financial
 
             // Saudi Arabia, Length: 24, BBAN: 2n,18c, Fields: SAkk bbcc cccc cccc cccc cccc (b = National bank code, c = Account number preceded by zeros, if required)
             { Country.SA, new Regex(@"^SA[0-9]{4}[0-9A-Z]{18}$", RegexOptions.Compiled) },
-
-            // Senegal, Length: 28, BBAN: 1a, 23n, Fields: SNkk annn nnnn nnnn nnnn nnnn nnnn (Nordea catalogued)
-            { Country.SN, new Regex(@"^SN[0-9]{2}[A-Z][0-9]{23}$", RegexOptions.Compiled) },
 
             // Serbia, Length: 22, BBAN: 18n, Fields: RSkk bbbc cccc cccc cccc xx (b = National bank code, c = Account number, x = Account check digits)
             { Country.RS, new Regex(@"^RS[0-9]{20}$", RegexOptions.Compiled) },
@@ -247,18 +214,97 @@ namespace Qowaiv.Financial
             // Turkey, Length: 26, BBAN: 5n,17c, Fields: TRkk bbbb bxcc cccc cccc cccc cc (b = National bank code, x = Reserved for future use (currently "0"), c = Account number)
             { Country.TR, new Regex(@"^TR[0-9]{7}[0-9A-Z]{17}$", RegexOptions.Compiled) },
 
-            // Ukraine, Length: 29, BBAN: 6n,19c, Fields: UAkk bbbb bbcc cccc cccc cccc cccc c (b = National bank code, c = Account number)
-            { Country.UA, new Regex(@"^UA[0-9]{8}[0-9A-Z]{19}$", RegexOptions.Compiled) },
-
             // United Arab Emirates, Length: 23, BBAN: 3n,16n, Fields: AEkk bbbc cccc cccc cccc ccc (b = National bank code, c = Account number)
             { Country.AE, new Regex(@"^AE[0-9]{5}[0-9]{16}$", RegexOptions.Compiled) },
 
             // United Kingdom, Length: 22, BBAN: 4a,14n, Fields: GBkk bbbb ssss sscc cccc cc (b = BIC bank code, s = Bank and branch code (sort code), c = Account number)
             { Country.GB, new Regex(@"^GB[0-9]{2}[A-Z]{4}[0-9]{14}$", RegexOptions.Compiled) },
 
+            // Vatican City, Length: 22, BBAN: 18n, Fields: VAkk bbbc cccc cccc cccc cc (b = National bank code, c = Account number)
+            { Country.VA, new Regex(@"^VA[0-9]{20}$", RegexOptions.Compiled) },
+
             // Virgin Islands, British, Length: 24, BBAN: 4c,16n, Fields: VGkk bbbb cccc cccc cccc cccc (b = National bank code, c = Account number)
             { Country.VG, new Regex(@"^VG[0-9]{2}[0-9A-Z]{4}[0-9]{16}$", RegexOptions.Compiled) },
 
+            #region Nordea has catalogued IBANs for countries listed below.
+
+            // Algeria, Length: 24, BBAN: 20n, Fields: DZkk nnnn nnnn nnnn nnnn nnnn (Nordea catalogued)
+            { Country.DZ, new Regex(@"^DZ[0-9]{22}$", RegexOptions.Compiled) },
+
+            // Angola, Length: 25, BBAN: 21n, Fields: AOkk nnnn nnnn nnnn nnnn nnnn n
+            { Country.AO, new Regex(@"^AO[0-9]{23}$", RegexOptions.Compiled) },
+            
+            // Benin, Length: 28, BBAN: 1a, 23n, Fields: BJkk annn nnnn nnnn nnnn nnnn nnnn
+            { Country.BJ, new Regex(@"^BJ[0-9]{2}[A-Z][0-9]{23}$", RegexOptions.Compiled) },
+            
+            // Burkina Faso, Length: 27, BBAN: 23n, Fields: BFkk nnnn nnnn nnnn nnnn nnnn nnn
+            { Country.BF, new Regex(@"^BF[0-9]{25}$", RegexOptions.Compiled) },
+            
+            // Burundi, Length: 16, BBAN: 12n, Fields: BIkk nnnn nnnn nnnn
+            { Country.BI, new Regex(@"^BI[0-9]{14}$", RegexOptions.Compiled) },
+            
+            // Cameroon, Length: 27, BBAN: 23n, Fields: CMkk nnnn nnnn nnnn nnnn nnnn nnn
+            { Country.CM, new Regex(@"^CM[0-9]{25}$", RegexOptions.Compiled) },
+            
+            // Cape Verde, Length: 25, BBAN: 21n, Fields: CVkk nnnn nnnn nnnn nnnn nnnn n
+            { Country.CV, new Regex(@"^CV[0-9]{23}$", RegexOptions.Compiled) },
+            
+            // Iran, Length: 26, BBAN: 22n, Fields: IRkk nnnn nnnn nnnn nnnn nnnn nn
+            { Country.IR, new Regex(@"^IR[0-9]{24}$", RegexOptions.Compiled) },
+
+            // Ivory Coast, Length: 28, BBAN: 1a, 23n, Fields: CIkk annn nnnn nnnn nnnn nnnn nnnn
+            { Country.CI, new Regex(@"^CI[0-9]{2}[A-Z]{23}$", RegexOptions.Compiled) },
+  
+            // Madagascar, Length: 27, BBAN: 23n, Fields: MGkk nnnn nnnn nnnn nnnn nnnn nnn
+            { Country.MG, new Regex(@"^MG[0-9]{25}$", RegexOptions.Compiled) },
+            
+            // Mali, Length: 28, BBAN: 1a, 23n, Fields: MLkk annn nnnn nnnn nnnn nnnn nnnn
+            { Country.ML, new Regex(@"^ML[0-9]{2}[A-Z][0-9]{23}$", RegexOptions.Compiled) },
+            
+            // Mozambique, Length: 25, BBAN: 21n, Fields: MZkk nnnn nnnn nnnn nnnn nnnn n
+            { Country.MZ, new Regex(@"^MZ[0-9]{23}$", RegexOptions.Compiled) },
+
+            // Senegal, Length: 28, BBAN: 1a, 23n, Fields: SNkk annn nnnn nnnn nnnn nnnn nnnn
+            { Country.SN, new Regex(@"^SN[0-9]{2}[A-Z][0-9]{23}$", RegexOptions.Compiled) },
+            
+            // Ukraine, Length: 29, BBAN: 6n,19c, Fields: UAkk bbbb bbcc cccc cccc cccc cccc c (b = National bank code, c = Account number)
+            { Country.UA, new Regex(@"^UA[0-9]{8}[0-9A-Z]{19}$", RegexOptions.Compiled) },
+
+            #endregion In the process of introducing the IBAN retrieved from SWIFT partner website
+
+            #region In the process of introducing the IBAN retrieved from SWIFT partner website 
+
+            // Comoros, Length: 27, BBAN: 23n, Fields: KMkk nnnn nnnn nnnn nnnn nnnn nnn
+            { Country.KM, new Regex(@"^KM[0-9]{25}$", RegexOptions.Compiled) },
+
+            // Chad, Length: 27, BBAN: 23n, Fields: KMkk nnnn nnnn nnnn nnnn nnnn nnn
+            { Country.TD, new Regex(@"^TD[0-9]{25}$", RegexOptions.Compiled) },
+
+            // Congo, Length: 27, BBAN: 23n, Fields: CGkk nnnn nnnn nnnn nnnn nnnn nnn
+            { Country.CG, new Regex(@"^CG[0-9]{25}$", RegexOptions.Compiled) },
+
+            // Egypt, Length: 27, BBAN: 23n, Fields: EGkk nnnn nnnn nnnn nnnn nnnn nnn
+            { Country.EG, new Regex(@"^EG[0-9]{25}$", RegexOptions.Compiled) },
+
+            // Gabon, Length: 27, BBAN: 23n, Fields: GAkk nnnn nnnn nnnn nnnn nnnn nnn
+            { Country.GA, new Regex(@"^GA[0-9]{25}$", RegexOptions.Compiled) },
+
+            // Honduras, Length: 28, BBAN: 4a, 20n, Fields: HNkk aaaa nnnn nnnn nnnn nnnn nnnn
+            { Country.HN, new Regex(@"^HN[0-9]{2}[A-Z]{4}[0-9]{20}$", RegexOptions.Compiled) },
+
+            // Morocco, Length: 28, BBAN: 24n, Fields: GAkk nnnn nnnn nnnn nnnn nnnn nnnn
+            { Country.MA, new Regex(@"^MA[0-9]{26}$", RegexOptions.Compiled) },
+
+            // Nicaragua, Length: 32, BBAN: 4a, 24n, Fields: NIkk aaaa nnnn nnnn nnnn nnnn nnnn nnnn
+            { Country.NI, new Regex(@"^NI[0-9]{2}[A-Z]{4}[0-9]{24}$", RegexOptions.Compiled) },
+
+            // Niger, Length: 28, BBAN: 2a, 22n, Fields: NEkk aann nnnn nnnn nnnn nnnn nnnn
+            { Country.NE, new Regex(@"^NE[0-9]{2}[A-Z]{2}[0-9]{22}$", RegexOptions.Compiled) },
+
+            // Togo, Length: 28, BBAN: 2a, 22n, Fields: TGkk aann nnnn nnnn nnnn nnnn nnnn
+            { Country.TG, new Regex(@"^TG[0-9]{2}[A-Z]{2}[0-9]{22}$", RegexOptions.Compiled) },
+
+            #endregion
         };
     }
 }

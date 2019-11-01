585f01e7f74d8dfc531bb90a70c139ca7659677c
Corniel Nobel <mail@corniel.nl>
2019-11-01 13:10:12 +0100

    Remove Qowaiv.Validation.* packages as the are now part of the qowaiv-validation repro. (#107)

--- test/Qowaiv.CodeGenerator.UnitTests/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-
-// Info about the assembly.
-[assembly: AssemblyTitle("Qowaiv.CodeGeration.UnitTests")]
-[assembly: AssemblyProduct("Qowaiv.CodeGeration.UnitTests")]
-[assembly: AssemblyDescription("Qowaiv Single Value Object library unit tests")]

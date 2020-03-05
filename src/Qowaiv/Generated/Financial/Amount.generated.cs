e9adae3c187fce43d35025b825aeaf8cf85677e3
Corniel Nobel <mail@corniel.nl>
2020-03-05 22:05:13 +0100

    Limited support for IConvertable.

--- src/Qowaiv/Generated/Financial/Amount.generated.cs
+++ src/Qowaiv/Generated/Financial/Amount.generated.cs
@@ -228,6 +228,46 @@ namespace Qowaiv.Financial
 namespace Qowaiv.Financial
 {
     using System;
+    using System.Collections.Generic;
+
+    public partial struct Amount : IConvertible
+    {
+        /// <inheritdoc/>
+        object IConvertible.ToType(Type conversionType, IFormatProvider provider) => Convertable.ToType(conversionType, provider);
+        /// <inheritdoc/>
+        bool IConvertible.ToBoolean(IFormatProvider provider) => Convertable.ToBoolean(provider);
+        /// <inheritdoc/>
+        byte IConvertible.ToByte(IFormatProvider provider) => Convertable.ToByte(provider);
+        /// <inheritdoc/>
+        char IConvertible.ToChar(IFormatProvider provider) => Convertable.ToChar(provider);
+        /// <inheritdoc/>
+        DateTime IConvertible.ToDateTime(IFormatProvider provider) => Convertable.ToDateTime(provider);
+        /// <inheritdoc/>
+        decimal IConvertible.ToDecimal(IFormatProvider provider) => Convertable.ToDecimal(provider);
+        /// <inheritdoc/>
+        double IConvertible.ToDouble(IFormatProvider provider) => Convertable.ToDouble(provider);
+        /// <inheritdoc/>
+        short IConvertible.ToInt16(IFormatProvider provider) => Convertable.ToInt16(provider);
+        /// <inheritdoc/>
+        int IConvertible.ToInt32(IFormatProvider provider) => Convertable.ToInt32(provider);
+        /// <inheritdoc/>
+        long IConvertible.ToInt64(IFormatProvider provider) => Convertable.ToInt64(provider);
+        /// <inheritdoc/>
+        sbyte IConvertible.ToSByte(IFormatProvider provider) => Convertable.ToSByte(provider);
+        /// <inheritdoc/>
+        float IConvertible.ToSingle(IFormatProvider provider) => Convertable.ToSingle(provider);
+        /// <inheritdoc/>
+        ushort IConvertible.ToUInt16(IFormatProvider provider) => Convertable.ToUInt16(provider);
+        /// <inheritdoc/>
+        uint IConvertible.ToUInt32(IFormatProvider provider) => Convertable.ToUInt32(provider);
+        /// <inheritdoc/>
+        ulong IConvertible.ToUInt64(IFormatProvider provider) => Convertable.ToUInt64(provider);
+    }
+}
+
+namespace Qowaiv.Financial
+{
+    using System;
     using System.Globalization;
 
     public partial struct Amount

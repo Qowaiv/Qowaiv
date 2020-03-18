063ed1603ebba6eefdc4dd915704fbbc9d01a554
Corniel Nobel <mail@corniel.nl>
2020-03-18 11:55:24 +0100

    A requirement for strongly typed-id's.

--- test/Qowaiv.UnitTests/Reflection/QowaivTypeTest.cs
+++ test/Qowaiv.UnitTests/Reflection/QowaivTypeTest.cs
@@ -1,5 +1,7 @@
 ﻿using NUnit.Framework;
 using Qowaiv.Reflection;
+using System;
+using System.Collections.Generic;
 
 namespace Qowaiv.UnitTests.Reflection
 {
@@ -28,5 +30,40 @@ namespace Qowaiv.UnitTests.Reflection
         {
             Assert.IsFalse(QowaivType.IsNullOrDefaultValue(new QowaivTypeTest()));
         }
+
+        [TestCase(typeof(string), "string")]
+        [TestCase(typeof(byte[]), "byte[]")]
+        [TestCase(typeof(Guid), "System.Guid")]
+        [TestCase(typeof(Dictionary<string, Action>), "System.Collections.Generic.Dictionary<string, System.Action>")]
+        [TestCase(typeof(long?), "long?")]
+        [TestCase(typeof(Dictionary<,>), "System.Collections.Generic.Dictionary<,>")]
+        [TestCase(typeof(Nullable<>), "System.Nullable<>")]
+        [TestCase(typeof(Dictionary<object, List<int?>>[]), "System.Collections.Generic.Dictionary<object, System.Collections.Generic.List<int?>>[]")]
+        [TestCase(typeof(NestedTest), "Qowaiv.UnitTests.Reflection.QowaivTypeTest.NestedTest")]
+        public void ToCSharpStringWithNamespace(Type type, string expected)
+        {
+            var formatted = QowaivType.ToCSharpString(type, true);
+            Assert.AreEqual(expected, formatted);
+        }
+
+        [TestCase(typeof(string), "string")]
+        [TestCase(typeof(byte[]), "byte[]")]
+        [TestCase(typeof(int[][]), "int[][]")]
+        [TestCase(typeof(int[,]), "int[,]")]
+        [TestCase(typeof(int[,,]), "int[,,]")]
+        [TestCase(typeof(Guid), "Guid")]
+        [TestCase(typeof(Dictionary<string, Action>), "Dictionary<string, Action>")]
+        [TestCase(typeof(long?), "long?")]
+        [TestCase(typeof(Dictionary<,>), "Dictionary<,>")]
+        [TestCase(typeof(Nullable<>), "Nullable<>")]
+        [TestCase(typeof(Dictionary<object, List<int?>>[]), "Dictionary<object, List<int?>>[]")]
+        [TestCase(typeof(NestedTest), "QowaivTypeTest.NestedTest")]
+        public void ToCSharpStringWithoutNamespace(Type type, string expected)
+        {
+            var formatted = QowaivType.ToCSharpString(type, false);
+            Assert.AreEqual(expected, formatted);
+        }
+
+        internal class NestedTest { }
     }
 }

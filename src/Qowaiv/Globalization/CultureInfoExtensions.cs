cc274c9ebc022d1c41a94b7789e538d468557b18
Corniel Nobel <mail@corniel.nl>
2020-04-04 11:18:32 +0200

    Introduction of the CultureInfo.Scoped() extension method. (#146)

---
+++ src/Qowaiv/Globalization/CultureInfoExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Globalization;
+
+namespace Qowaiv.Globalization
+{
+    /// <summary>Extensions on <see cref="CultureInfo"/>.</summary>
+    public static class CultureInfoExtensions
+    {
+        /// <summary>Gets a <see cref="CultureInfoScope"/> based on the <see cref="CultureInfo"/>.</summary>
+        public static CultureInfoScope Scoped(this CultureInfo culture) => new CultureInfoScope(culture);
+    }
+}
---
+++ test/Qowaiv.UnitTests/Globalization/CultureInfoScopeTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System.Globalization;
+using Qowaiv.Globalization;
+
+namespace Qowaiv.UnitTests.Globalization
+{
+    public class CultureInfoScopeTest
+    {
+        [Test]
+        public void Scoped_Ctor()
+        {
+            var current = CultureInfo.CurrentCulture;
+            var currentUI = CultureInfo.CurrentUICulture;
+
+            using (new CultureInfoScope("es-ES", "fr-FR"))
+            {
+                Assert.AreEqual("es-ES", CultureInfo.CurrentCulture.Name);
+                Assert.AreEqual("fr-FR", CultureInfo.CurrentUICulture.Name);
+            }
+
+            Assert.AreEqual(current, CultureInfo.CurrentCulture);
+            Assert.AreEqual(currentUI, CultureInfo.CurrentUICulture);
+        }
+
+        [Test]
+        public void Scoped_ExtensionMethod()
+        {
+            var current = CultureInfo.CurrentCulture;
+            var currentUI = CultureInfo.CurrentUICulture;
+
+            using (new CultureInfo("es-ES").Scoped())
+            {
+                Assert.AreEqual("es-ES", CultureInfo.CurrentCulture.Name);
+                Assert.AreEqual("es-ES", CultureInfo.CurrentUICulture.Name);
+            }
+
+            Assert.AreEqual(current, CultureInfo.CurrentCulture);
+            Assert.AreEqual(currentUI, CultureInfo.CurrentUICulture);
+        }
+    }
+}

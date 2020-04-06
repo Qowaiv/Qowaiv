5e403480ebef45857fbf042bb367f839113d8c6e
Corniel Nobel <mail@corniel.nl>
2020-04-06 11:48:08 +0200

    Improvements on SingleValueObjectAttribute.

--- src/Qowaiv/Mathematics/Fraction.cs
+++ src/Qowaiv/Mathematics/Fraction.cs
@@ -21,6 +21,7 @@ namespace Qowaiv.Mathematics
     /// <summary>Represents a fraction.</summary>
     [DebuggerDisplay("{DebuggerDisplay}")]
     [Serializable]
+    [SingleValueObject(SingleValueStaticOptions.Continuous, typeof(Tuple<long, long>))]
     [OpenApiDataType(description: "Faction", type: "string", format: "faction", pattern: "-?[0-9]+(/[0-9]+)?")]
     [TypeConverter(typeof(FractionTypeConverter))]
     [StructLayout(LayoutKind.Sequential)]

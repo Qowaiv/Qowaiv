4f825fc917de89405178b27f5eedcf95786e74cd
Corniel Nobel <mail@corniel.nl>
2021-10-28 12:35:54 +0200

    Add documentation.

--- src/Qowaiv.TestTools/Diagnostics/Contracts/AssertionAttribute.cs
+++ src/Qowaiv.TestTools/Diagnostics/Contracts/AssertionAttribute.cs
@@ -4,7 +4,9 @@ using System.Diagnostics;
 
 namespace Qowaiv.TestTools.Diagnostics.Contracts
 {
-    /// <summary>To mark a method explicitly as impure.</summary>
+    /// <summary>To mark a method explicitly as impure. Methods decorated with
+    /// this attribute do an assertion. The returned value allows continuation.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
     [Conditional("CONTRACTS_FULL")]
     public sealed class AssertionAttribute : ImpureAttribute { }
--- src/Qowaiv/Diagnostics/Contracts/CollectionMutationAttribute.cs
+++ src/Qowaiv/Diagnostics/Contracts/CollectionMutationAttribute.cs
@@ -3,7 +3,9 @@ using System.Diagnostics;
 
 namespace Qowaiv.Diagnostics.Contracts
 {
-    /// <summary>To mark a method explicitly as impure.</summary>
+    /// <summary>To mark a method explicitly as impure. Methods decorated with
+    /// this attribute return info about (like, removal or addition was successful).
+    /// </summary>
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
     [Conditional("CONTRACTS_FULL")]
     public sealed class CollectionMutationAttribute : ImpureAttribute { }

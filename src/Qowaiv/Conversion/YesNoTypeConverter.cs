43933adbc840bfedaab70ab1dd248ba7c7b65d04
Corniel Nobel <mail@corniel.nl>
2021-11-04 20:36:22 +0100

    YesNo can also convert from and to boolean. (#208)

--- src/Qowaiv/Conversion/YesNoTypeConverter.cs
+++ src/Qowaiv/Conversion/YesNoTypeConverter.cs
@@ -1,3 +1,5 @@
+using System;
+using System.ComponentModel;
 using System.Diagnostics.Contracts;
 using System.Globalization;
 
@@ -6,6 +8,40 @@ namespace Qowaiv.Conversion
     /// <summary>Provides a conversion for a Yes-no.</summary>
     public class YesNoTypeConverter : SvoTypeConverter<YesNo>
     {
+        /// <inheritdoc/>
+        [Pure]
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+            => sourceType == typeof(bool)
+            || base.CanConvertFrom(context, sourceType);
+
+        /// <inheritdoc/>
+        [Pure]
+        public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
+            => destinationType == typeof(bool)
+            || base.CanConvertTo(context, destinationType);
+
+        /// <inheritdoc/>
+        [Pure]
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            if (value is bool boolean)
+            {
+                return (YesNo)boolean;
+            }
+            else return base.ConvertFrom(context, culture, value);
+        }
+
+        /// <inheritdoc/>
+        [Pure]
+        public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+        {
+            if (destinationType == typeof(bool) && value is YesNo yesNo)
+            {
+                return (bool)yesNo;
+            }
+            else return base.ConvertTo(context, culture, value, destinationType);
+        }
+
         /// <inheritdoc/>
         [Pure]
         protected override YesNo FromString(string str, CultureInfo culture) => YesNo.Parse(str, culture);

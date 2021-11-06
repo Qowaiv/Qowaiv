e10d53607def214373e2da2a5a31019e3c459496
Corniel Nobel <mail@corniel.nl>
2021-11-06 12:33:32 +0100

    Added missing specs for humanizer craetor extensions for Fraction.

---
+++ specs/Qowaiv.Specs/Mathematics/Fraction_specs.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Qowaiv.Mathematics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathematics.Fraction_specs
+{
+    public class Has_humanzizer_creators
+    {
+        [Test]
+        public void int_DividedBy_int()
+            => 12.DividedBy(24).Should().Be(new Fraction(1, 2));
+
+        [Test]
+        public void long_DividedBy_long()
+           => 12L.DividedBy(24L).Should().Be(new Fraction(1, 2));
+
+        [Test]
+        public void Fraction_from_double()
+            => 0.5.Fraction().Should().Be(new Fraction(1, 2));
+
+        [Test]
+        public void Fraction_from_decimal()
+            => 0.5m.Fraction().Should().Be(new Fraction(1, 2));
+    }
+}

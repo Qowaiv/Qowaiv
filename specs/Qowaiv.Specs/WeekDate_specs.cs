72873f1da902d96cba1087e5e795c1b37ce6e062
Corniel Nobel <mail@corniel.nl>
2021-11-06 10:30:22 +0100

    Introduction of Qowaiv.Security.Secret (#200)

--- specs/Qowaiv.Specs/WeekDate_specs.cs
+++ specs/Qowaiv.Specs/WeekDate_specs.cs
@@ -20,7 +20,7 @@ namespace WeekDate_specs
         {
             using (TestCultures.En_GB.Scoped())
             {
-                Converting.To<WeekDate>().From(null).Should().Be(default);
+                Converting.From<string>(null).To<WeekDate>().Should().Be(default);
             }
         }
 
@@ -29,7 +29,7 @@ namespace WeekDate_specs
         {
             using (TestCultures.En_GB.Scoped())
             {
-                Converting.To<WeekDate>().From("2017-W23-7").Should().Be(Svo.WeekDate);
+                Converting.From("2017-W23-7").To<WeekDate>().Should().Be(Svo.WeekDate);
             }
         }
 
@@ -38,40 +38,40 @@ namespace WeekDate_specs
         {
             using (TestCultures.En_GB.Scoped())
             {
-                Converting.Value(Svo.WeekDate).ToString().Should().Be("2017-W23-7");
+                Converting.ToString().From(Svo.WeekDate).Should().Be("2017-W23-7");
             }
         }
 
         [Test]
         public void from_Date()
-            => Converting.To<WeekDate>().From(new Date(2017, 06, 11)).Should().Be(Svo.WeekDate);
+            => Converting.From(new Date(2017, 06, 11)).To<WeekDate>().Should().Be(Svo.WeekDate);
 
         [Test]
         public void from_DateTime()
-            => Converting.To<WeekDate>().From(new DateTime(2017, 06, 11)).Should().Be(Svo.WeekDate);
+            => Converting.From(new DateTime(2017, 06, 11)).To<WeekDate>().Should().Be(Svo.WeekDate);
 
         [Test]
         public void from_DateTimeOffset()
-            => Converting.To<WeekDate>().From(new DateTimeOffset(2017, 06, 11, 00, 00, 00, TimeSpan.Zero)).Should().Be(Svo.WeekDate);
+            => Converting.From(new DateTimeOffset(2017, 06, 11, 00, 00, 00, TimeSpan.Zero)).To<WeekDate>().Should().Be(Svo.WeekDate);
 
         [Test]
         public void from_LocalDateTime()
-            => Converting.To<WeekDate>().From(new LocalDateTime(2017, 06, 11)).Should().Be(Svo.WeekDate);
+            => Converting.From(new LocalDateTime(2017, 06, 11)).To<WeekDate>().Should().Be(Svo.WeekDate);
 
         [Test]
         public void to_Date()
-            => Converting.Value(Svo.WeekDate).To<Date>().Should().Be(new Date(2017, 06, 11));
+            => Converting.To<Date>().From(Svo.WeekDate).Should().Be(new Date(2017, 06, 11));
 
         [Test]
         public void to_DateTime()
-            => Converting.Value(Svo.WeekDate).To<DateTime>().Should().Be(new DateTime(2017, 06, 11));
+            => Converting.To<DateTime>().From(Svo.WeekDate).Should().Be(new DateTime(2017, 06, 11));
 
         [Test]
         public void to_DateTimeOffset()
-            => Converting.Value(Svo.WeekDate).To<DateTimeOffset>().Should().Be(new DateTimeOffset(2017, 06, 11, 00, 00, 00, TimeSpan.Zero));
+            => Converting.To<DateTimeOffset>().From(Svo.WeekDate).Should().Be(new DateTimeOffset(2017, 06, 11, 00, 00, 00, TimeSpan.Zero));
 
         [Test]
         public void to_LocalDateTime()
-            => Converting.Value(Svo.WeekDate).To<LocalDateTime>().Should().Be(new LocalDateTime(2017, 06, 11));
+            => Converting.To<LocalDateTime>().From(Svo.WeekDate).Should().Be(new LocalDateTime(2017, 06, 11));
     }
 }
---
+++ src/Qowaiv/Security/Secret.cs
@@ -0,0 +1,90 @@
+﻿using Qowaiv.Conversion.Security;
+using Qowaiv.Diagnostics;
+using Qowaiv.Hashing;
+using Qowaiv.Security.Cryptography;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Qowaiv.Security
+{
+    /// <summary>Represents a secret.</summary>
+    [TypeConverter(typeof(SecretTypeConverter))]
+    [DebuggerDisplay("{DebuggerDisplay}")]
+    public readonly struct Secret : IEquatable<Secret>
+    {
+        /// <summary>Represents an empty/not set secret.</summary>
+        public static readonly Secret Empty;
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private readonly string m_Value;
+
+        private Secret(string value) => m_Value = value;
+
+        /// <summary>Returns true if the secret is empty/not set.</summary>
+        [Pure]
+        public bool IsEmpty() => m_Value is null;
+
+        /// <summary>Gets a string representing this secret.</summary>
+        [Pure]
+        public string Value() => m_Value ?? string.Empty;
+
+        /// <summary>Computes a <see cref="CryptographicSeed"/> for the secret.</summary>
+        /// <param name="algorithm">
+        /// The algorithm to 
+        /// </param>
+        [Pure]
+        public CryptographicSeed ComputeHash(HashAlgorithm algorithm)
+            => algorithm.ComputeCryptographicSeed(Encoding.UTF8.GetBytes(Value()));
+
+        /// <inheritdoc />
+        [Pure]
+        public override bool Equals(object obj) => obj is Secret other && Equals(other);
+
+        /// <summary>Returns true if both are empty, otherwise false.</summary>
+        /// <remarks>
+        /// Secrets are supposed to be passed around, without knowing its content.
+        /// </remarks>
+        [Pure]
+        public bool Equals(Secret other) => IsEmpty() && other.IsEmpty();
+
+        /// <inheritdoc />
+        [Pure]
+        public override int GetHashCode() => Hash.NotSupportedBy<Secret>();
+
+        /// <summary>Represents the secret as "*****".</summary>
+        /// <remarks>
+        /// To prevent unintended exposure. 
+        /// </remarks>
+        [Pure]
+        public override string ToString() => m_Value is null ? string.Empty : "*****";
+
+        /// <summary>Converts the secret to a JSON null node.</summary>
+        /// <remarks>
+        /// To prevent unintended exposure. 
+        /// </remarks>
+        [Pure]
+        public object ToJson() => null;
+
+        /// <summary>Returns a <see cref="string" /> that represents the current date span for debug purposes.</summary>
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private string DebuggerDisplay => m_Value is null ? DebugDisplay.Empty : Value();
+
+        /// <summary>Creates a secret from an UTF8 string.</summary >
+        /// <param name="str" >
+        /// A secret describing a cryptographic seed.
+        /// </param >
+        [Pure]
+        public static Secret Parse(string str)
+            => string.IsNullOrEmpty(str)
+            ? Empty
+            : new(str);
+     
+        /// <summary>Creates a secret from a JSON string node.</summary>
+        [Pure]
+        public static Secret FromJson(string json) => Parse(json);
+    }
+}

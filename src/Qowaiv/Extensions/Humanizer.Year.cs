ddc41130939289dfffbb8697d1a85dbde2d6e14d
Corniel Nobel <mail@corniel.nl>
2021-11-11 14:01:38 +0100

    Revisit some (breaking) changes (#219)

--- src/Qowaiv/Extensions/Humanizer.Year.cs
+++ src/Qowaiv/Extensions/Humanizer.Year.cs
@@ -1,9 +1,9 @@
 ﻿namespace Qowaiv;
 
-/// <summary>Extensions to create <see cref="Qowaiv.Year"/>s, inspired by Humanizer.NET.</summary>
+/// <summary>Extensions to create <see cref="Year"/>s, inspired by Humanizer.NET.</summary>
 public static class NumberToYearExtensions
 {
-    /// <summary>Create a <see cref="Qowaiv.Year"/> from a <see cref="int"/>.</summary>
+    /// <summary>Create a <see cref="Year"/> from a <see cref="int"/>  as Common Era. (also known as Anno Domini).</summary>
     [Pure]
-    public static Year Year(this int year) => Qowaiv.Year.Create(year);
+    public static Year CE(this int year) => Year.Create(year);
 }

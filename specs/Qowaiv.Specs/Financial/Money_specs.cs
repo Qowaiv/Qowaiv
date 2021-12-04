c7ab56c82011ed9ca9d8849ca122af983b392078
Corniel Nobel <mail@corniel.nl>
2021-12-04 08:23:04 +0100

    Money divided by money gives ratio. (#222)

--- specs/Qowaiv.Specs/Financial/Money_specs.cs
+++ specs/Qowaiv.Specs/Financial/Money_specs.cs
@@ -1,35 +1,43 @@
-﻿using FluentAssertions;
-using NUnit.Framework;
-using Qowaiv.Financial;
-using Qowaiv.Specs;
-using Qowaiv.TestTools;
-using Qowaiv.TestTools.Globalization;
-using System.Globalization;
+﻿namespace Financial.Money_specs;
 
-namespace Financial.Money_specs
+public class Supports_type_conversion
 {
-    public class Supports_type_conversion
+    [Test]
+    public void via_TypeConverter_registered_with_attribute()
+       => typeof(Money).Should().HaveTypeConverterDefined();
+
+    [Test]
+    public void from_string()
     {
-        [Test]
-        public void via_TypeConverter_registered_with_attribute()
-           => typeof(Money).Should().HaveTypeConverterDefined();
-
-        [Test]
-        public void from_string()
+        using (TestCultures.En_GB.Scoped())
         {
-            using (TestCultures.En_GB.Scoped())
-            {
-                Converting.From("€42.17").To<Money>().Should().Be(Svo.Money);
-            }
+            Converting.From("€42.17").To<Money>().Should().Be(Svo.Money);
         }
+    }
 
-        [Test]
-        public void to_string()
+    [Test]
+    public void to_string()
+    {
+        using (TestCultures.En_GB.Scoped())
         {
-            using (TestCultures.En_GB.Scoped())
-            {
-                Converting.ToString().From(Svo.Money).Should().Be("€42.17");
-            }
+            Converting.ToString().From(Svo.Money).Should().Be("€42.17");
         }
+    }
+}
+
+public class Has_operators
+{
+    [Test]
+    public void to_divide_money_by_money_with_same_currency_as_decimal()
+    {
+        var ratio = Svo.Money / (16 + Currency.EUR);
+        ratio.Should().Be(2.635625m);
+    }
+
+    [Test]
+    public void to_divide_money_by_money_with_diffrent_currencies_is_not_supported()
+    {
+        Func<decimal> division =  () => Svo.Money / (16 + Currency.USD);
+        division.Should().Throw<CurrencyMismatchException>();
     }
 }

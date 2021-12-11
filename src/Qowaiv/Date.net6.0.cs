180b81506a3aaefd7136cac2177fe08fd57b13c8
Corniel Nobel <mail@corniel.nl>
2021-12-11 17:55:49 +0100

    Support casts between Qowaiv.Date and System.DateOnly (#221)

---
+++ src/Qowaiv/Date.net6.0.cs
@@ -0,0 +1,13 @@
+﻿#if NET6_0_OR_GREATER
+
+namespace Qowaiv;
+
+public partial struct Date
+{
+    /// <summary>Casts a <see cref="Date"/> implictly to a <see cref="DateOnly"/>.</summary>
+    public static implicit operator DateOnly(Date date) => new(date.Year, date.Month, date.Day);
+
+    /// <summary>Casts a <see cref="DateOnly"/> explicitly to a <see cref="Date"/>.</summary>
+    public static explicit operator Date(DateOnly date) => new(date.Year, date.Month, date.Day);
+}
+#endif

8af31ceddedc7aba4f648550f3f11f4095cef5cf
Corniel Nobel <mail@corniel.nl>
2022-01-07 11:16:01 +0100

    Get warnings.

---
+++ src/Qowaiv/Dummy.cs
@@ -0,0 +1,12 @@
+﻿namespace Qowaiv;
+
+public class Dummy : IDisposable
+{
+    public void X() => throw new NullReferenceException();
+
+
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+}

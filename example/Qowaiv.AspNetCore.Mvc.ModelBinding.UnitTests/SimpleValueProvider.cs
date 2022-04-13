3f09ee4d111bef5bddbdf93d57a96113aab8cb02
Corniel Nobel <mail@corniel.nl>
2022-04-13 17:55:07 +0200

    Solve issues with Global usings (#238)

--- example/Qowaiv.AspNetCore.Mvc.ModelBinding.UnitTests/SimpleValueProvider.cs
+++ example/Qowaiv.AspNetCore.Mvc.ModelBinding.UnitTests/SimpleValueProvider.cs
@@ -3,56 +3,55 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 
-namespace Qowaiv.AspNetCore.Mvc.ModelBinding.UnitTests
+namespace Qowaiv.AspNetCore.Mvc.ModelBinding.UnitTests;
+
+public sealed class SimpleValueProvider : Dictionary<string, object>, IValueProvider
 {
-    public sealed class SimpleValueProvider : Dictionary<string, object>, IValueProvider
+    private readonly CultureInfo _culture;
+
+    public SimpleValueProvider() : this(null) { }
+
+    public SimpleValueProvider(CultureInfo culture) : base(StringComparer.OrdinalIgnoreCase)
     {
-        private readonly CultureInfo _culture;
+        _culture = culture ?? CultureInfo.InvariantCulture;
+    }
 
-        public SimpleValueProvider() : this(null) { }
-
-        public SimpleValueProvider(CultureInfo culture) : base(StringComparer.OrdinalIgnoreCase)
+    public bool ContainsPrefix(string prefix)
+    {
+        foreach (string key in Keys)
         {
-            _culture = culture ?? CultureInfo.InvariantCulture;
-        }
-
-        public bool ContainsPrefix(string prefix)
-        {
-            foreach (string key in Keys)
+            if (ModelStateDictionary.StartsWithPrefix(prefix, key))
             {
-                if (ModelStateDictionary.StartsWithPrefix(prefix, key))
-                {
-                    return true;
-                }
+                return true;
             }
-
-            return false;
         }
 
-        public ValueProviderResult GetValue(string key)
+        return false;
+    }
+
+    public ValueProviderResult GetValue(string key)
+    {
+        if (TryGetValue(key, out var rawValue))
         {
-            if (TryGetValue(key, out var rawValue))
+            if (rawValue != null && rawValue.GetType().IsArray)
             {
-                if (rawValue != null && rawValue.GetType().IsArray)
-                {
-                    var array = (Array)rawValue;
+                var array = (Array)rawValue;
 
-                    var stringValues = new string[array.Length];
-                    for (var i = 0; i < array.Length; i++)
-                    {
-                        stringValues[i] = array.GetValue(i) as string ?? Convert.ToString(array.GetValue(i), _culture);
-                    }
-
-                    return new ValueProviderResult(stringValues, _culture);
-                }
-                else
+                var stringValues = new string[array.Length];
+                for (var i = 0; i < array.Length; i++)
                 {
-                    var stringValue = rawValue as string ?? Convert.ToString(rawValue, _culture) ?? string.Empty;
-                    return new ValueProviderResult(stringValue, _culture);
+                    stringValues[i] = array.GetValue(i) as string ?? Convert.ToString(array.GetValue(i), _culture);
                 }
+
+                return new ValueProviderResult(stringValues, _culture);
             }
-
-            return ValueProviderResult.None;
+            else
+            {
+                var stringValue = rawValue as string ?? Convert.ToString(rawValue, _culture) ?? string.Empty;
+                return new ValueProviderResult(stringValue, _culture);
+            }
         }
+
+        return ValueProviderResult.None;
     }
 }

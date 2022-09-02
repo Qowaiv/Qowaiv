97f15b14999c07e2e995f104cff17449bd594401
Corniel Nobel <mail@corniel.nl>
2022-09-02 10:36:08 +0200

    Humanize(r)!

---
+++ src/Qowaiv/Extensions/Humanizer.Chemistry.CasRegistryNumber.cs
@@ -0,0 +1,13 @@
+﻿namespace Qowaiv.Chemistry;
+
+/// <summary>Extensions to create <see cref="CasRegistryNumber"/>s, inspired by Humanizer.NET.</summary>
+public static class NumberToEloExtensions
+{
+    /// <summary>Interprets the <see cref="int"/> if it was written as a CAS Registry Number.</summary>
+    [Pure]
+    public static CasRegistryNumber CasNr(this int number) => CasRegistryNumber.Create(number);
+
+    /// <summary>Interprets the <see cref="long"/> if it was written as a CAS Registry Number.</summary>
+    [Pure]
+    public static CasRegistryNumber CasNr(this long number) => CasRegistryNumber.Create(number);
+}

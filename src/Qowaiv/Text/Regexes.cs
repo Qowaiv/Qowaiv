4c3b7815ffa340c75fbef785e0f36d648d8f03d1
Corniel Nobel <mail@corniel.nl>
2022-11-12 20:42:58 +0100

    Change text.

--- src/Qowaiv/Text/Regexes.cs
+++ src/Qowaiv/Text/Regexes.cs
@@ -4,7 +4,7 @@ internal static class Regexes
 {
     /// <summary>
     /// To prevent DoS attacks exploiting regular expressions on user input,
-    /// the match time-out is set 0.1 ms.
+    /// the match time-out is set 1 ms.
     /// </summary>
     public static readonly TimeSpan MatchTimeout = TimeSpan.FromMilliseconds(1);
 }

1ee96ad0f8f18651099cf87ab7e638903bea1146
Corniel Nobel <mail@corniel.nl>
2023-01-17 22:02:39 +0100

    binary roundtrip.

---
+++ specs/Qowaiv.Specs/TestTools/Serialize_deserialize_helper_specs.cs
@@ -0,0 +1,12 @@
+﻿namespace TestTools.Serialize_deserialize_helper_specs;
+
+public class Fails_on
+{
+    [Test]
+    [Obsolete("Usage of the binary formatter is considered harmful.")]
+    public void binary_roundtrip_null()
+    {
+        Func<object> roundtrip = () => SerializeDeserialize.Binary<object>(null);
+        roundtrip.Should().Throw<ArgumentNullException>();
+    }
+}

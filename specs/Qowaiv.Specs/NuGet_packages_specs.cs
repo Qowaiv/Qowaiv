15286a127bbb066e641f612867309546af638132
Corniel Nobel <corniel@gmail.com>
2023-07-31 16:05:51 +0200

    Ensure analyzers are defined as  PrivateAssets.

--- specs/Qowaiv.Specs/NuGet_packages_specs.cs
+++ specs/Qowaiv.Specs/NuGet_packages_specs.cs
@@ -1,20 +1,33 @@
-﻿namespace NuGet_packages_specs
-{
-    public class All_NuGet_Packages
-    {
-        [TestCaseSource(nameof(Packages))]
-        public void Are_signed(Assembly package)
-            => package.Should().HavePublicKey(
-                "0024000004800000940000000602000000240000525341310004000001000100EF35DF58AA7FEC73A11E70572E6B3791601006EF3FB1C6C1F1A402BA83BB2EDC975C61E8A32D792EDB864127F0D2C67EB7A64A9D3A0CDB0B1BB37FF2D0FCFD7990304623C044439D04DAC49624CC6D7937581419D995C2689F9898EC09C941B3EB3CAB8E4FC8F90B4AE5D45AB03D691D4D1F4B68450DAD41FED46671376934B0");
+﻿namespace NuGet_packages_specs;
 
-        private static IEnumerable<Assembly> Packages
+public class All_NuGet_Packages
+{
+    [TestCaseSource(nameof(Packages))]
+    public void Are_signed(Assembly package)
+        => package.Should().HavePublicKey(
+            "0024000004800000940000000602000000240000525341310004000001000100EF35DF58AA7FEC73A11E70572E6B3791601006EF3FB1C6C1F1A402BA83BB2EDC975C61E8A32D792EDB864127F0D2C67EB7A64A9D3A0CDB0B1BB37FF2D0FCFD7990304623C044439D04DAC49624CC6D7937581419D995C2689F9898EC09C941B3EB3CAB8E4FC8F90B4AE5D45AB03D691D4D1F4B68450DAD41FED46671376934B0");
+
+    [TestCaseSource(nameof(Packages))]
+    public void x(Assembly package)
+    {
+        var references = package.GetReferencedAssemblies();
+
+        foreach(var reference in references) 
         {
-            get
-            {
-                yield return typeof(Qowaiv.SingleValueObjectAttribute).Assembly;
-                yield return typeof(Qowaiv.Data.SvoParameter).Assembly;
-                yield return typeof(Qowaiv.TestTools.JsonTester).Assembly;
-            }
+            Console.WriteLine(reference);
+        }
+
+        references.Should().BeEmpty();
+    }
+
+
+    private static IEnumerable<Assembly> Packages
+    {
+        get
+        {
+            yield return typeof(Qowaiv.SingleValueObjectAttribute).Assembly;
+            yield return typeof(Qowaiv.Data.SvoParameter).Assembly;
+            yield return typeof(Qowaiv.TestTools.JsonTester).Assembly;
         }
     }
 }

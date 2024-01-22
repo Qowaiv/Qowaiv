ca3cd0bd96fee13556b35c2d047ae43db8ef51e0
Corniel Nobel <corniel@gmail.com>
2024-01-22 12:41:09 +0100

    Not needed anymore.

--- src/Qowaiv/Hashing/HashingNotSupported.cs
+++ src/Qowaiv/Hashing/HashingNotSupported.cs
@@ -5,9 +5,7 @@
 /// </summary>
 [ExcludeFromCodeCoverage]
 [Serializable]
-#pragma warning disable S3925 // "ISerializable" should be implemented correctly
 public class HashingNotSupported : NotSupportedException
-#pragma warning restore S3925 // "ISerializable" should be implemented correctly
 {
     /// <summary>Initializes a new instance of the <see cref="HashingNotSupported"/> class.</summary>
     [ExcludeFromCodeCoverage/* Justification = Required for extensibility. */]

eb74b8e064dae0676a364952c3e0501be0d13f1f
Corniel Nobel <corniel@gmail.com>
2024-02-14 13:35:37 +0100

    Implement INumber onFraction (#373)
    Implement INumber for Fraction.

---
+++ specs/Qowaiv.Specs/TestTools/RandomFractionAttribute.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework.Interfaces;
+using NUnit.Framework.Internal;
+
+namespace Qowaiv.Specs.TestTools;
+[AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
+public sealed class RandomFractionAttribute(int count, bool includeZero = false) : NUnitAttribute, IParameterDataSource
+{
+    public int Count { get; } = count;
+
+    public bool IncludeZero { get; } = includeZero;
+
+    /// <inheritdoc />
+    [Pure]
+    public IEnumerable GetData(IParameterInfo parameter)
+    {
+        var rnd = Randomizer.GetRandomizer(parameter.ParameterInfo);
+
+        return Enumerable.Range(0, Count).Select(_ => Fraction(rnd));
+
+        Fraction Fraction(Randomizer rnd)
+        {
+            var num = rnd.Next(-255, 255) * rnd.Next(255);
+            while (num == 0 && !IncludeZero)
+            {
+                num = rnd.Next(-255, 255) * rnd.Next(255);
+            }
+            var den = rnd.Next(1, 255) * rnd.Next(1, 255);
+            return num.DividedBy(den);
+        }
+    }
+}

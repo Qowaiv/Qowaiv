53c8e4257bd35c0f102ea21e2b1423951d6108b3
Corniel Nobel <corniel@gmail.com>
2024-02-18 11:00:42 +0100

    Single class per file.

---
+++ src/Qowaiv.Diagnostics.Contracts/EmptyInterfaceAttribute.cs
@@ -0,0 +1,6 @@
+﻿namespace Qowaiv.Diagnostics.Contracts;
+
+/// <summary>Indicates the class is empty by design.</summary>
+[AttributeUsage(AttributeTargets.Interface, AllowMultiple = false)]
+[Conditional("CONTRACTS_FULL")]
+public class EmptyInterfaceAttribute(string justification) : EmptyTypeAttribute(justification) { }

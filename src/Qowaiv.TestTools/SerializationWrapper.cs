656aef7f411dda4b0026196dc779b76b738bff0e
Corniel Nobel <corniel@gmail.com>
2024-03-11 17:18:10 +0100

    Montspan subtract improvement (#378)

--- src/Qowaiv.TestTools/SerializationWrapper.cs
+++ src/Qowaiv.TestTools/SerializationWrapper.cs
@@ -3,10 +3,11 @@
 /// <summary>Wrapper used by <see cref="Serialize.Xml{T}(T)"/>
 /// and <see cref="SerializeDeserialize.Xml{T}(T)"/>.
 /// </summary>
+[Mutable]
 [Serializable]
 [XmlRoot("Wrapper")]
 public sealed class SerializationWrapper<T>
 {
     /// <summary>The generic part of the wrapper.</summary>
-    public T? Value { get; init; }
+    public T? Value { get; set; }
 }

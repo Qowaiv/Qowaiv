724dbf31d0a6d81ddd511a2f6a63d88a5ee399e1
Corniel Nobel <corniel@gmail.com>
2024-07-02 11:55:56 +0200

    ISpanFormattable INumbers should be able to provide formatting (#393)

---
+++ specs/Qowaiv.Specs/Formatting/String_interpolation_specs.cs
@@ -0,0 +1,30 @@
+﻿namespace Formatting.String_interpolation_specs;
+
+[SetCulture("en-US")]
+public class Formats
+{
+    [Test]
+    public void Amount()
+    {
+        var formatted = 14.17.Amount().ToString("C");
+        var interpolated = $"{14.17.Amount():C}";
+        interpolated.Should().Be(formatted);
+    }
+
+    [Test]
+    public void Percentage()
+    {
+        var formatted = 14.17.Percent().ToString();
+        var interpolated = $"{14.17.Percent()}";
+        interpolated.Should().Be(formatted);
+    }
+
+    [Test]
+    public void Fraction()
+    {
+        var fraction = 4.DividedBy(17);
+        var formatted = fraction.ToString();
+        var interpolated = $"{fraction}";
+        interpolated.Should().Be(formatted);
+    }
+}

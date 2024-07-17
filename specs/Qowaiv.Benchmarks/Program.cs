3b46e9d07a4975c6285259664d77cc2ae98f6b7a
Corniel Nobel <corniel@gmail.com>
2024-07-17 11:03:41 +0200

    Add benchmark.

--- specs/Qowaiv.Benchmarks/Program.cs
+++ specs/Qowaiv.Benchmarks/Program.cs
@@ -6,15 +6,16 @@ public static class Program
 {
     public static void Main()
     {
-        BenchmarkRunner.Run<DecimalBenchmark>();
+        BenchmarkRunner.Run<EmailBenchmark>();
+        //BenchmarkRunner.Run<DecimalBenchmark>();
 
-        BenchmarkRunner.Run<IbanBenchmark.ParseUnformatted>();
-        BenchmarkRunner.Run<IbanBenchmark.ParseFormatted>();
+        //BenchmarkRunner.Run<IbanBenchmark.ParseUnformatted>();
+        //BenchmarkRunner.Run<IbanBenchmark.ParseFormatted>();
 
-        BenchmarkRunner.Run<RoundBenchmark>();
+        //BenchmarkRunner.Run<RoundBenchmark>();
 
-        BenchmarkRunner.Run<UuidBenchmark.Parse>();
-        BenchmarkRunner.Run<UuidBenchmark.StringOutput>();
-        BenchmarkRunner.Run<UuidBenchmark.Version>();
+        //BenchmarkRunner.Run<UuidBenchmark.Parse>();
+        //BenchmarkRunner.Run<UuidBenchmark.StringOutput>();
+        //BenchmarkRunner.Run<UuidBenchmark.Version>();
     }
 }

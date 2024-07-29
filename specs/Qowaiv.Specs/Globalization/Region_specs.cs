299b251b4753fdabf0c26f6a65aacc3cc9eaea2d
Corniel Nobel <corniel@gmail.com>
2024-07-29 21:03:58 +0200

    Introduction of regions

---
+++ specs/Qowaiv.Specs/Globalization/Region_specs.cs
@@ -0,0 +1,19 @@
+﻿using System.Resources;
+
+namespace Globalization.Region_specs;
+
+public class Resources_
+{
+    public Resources_()
+    {
+        var additional = new ResourceManager("Qowaiv.Specs.Globalization.AdditionalRegions", typeof(Resources_).Assembly);
+        Region.RegisterAdditional(additional);
+    }
+
+    [Test]
+    public void All()
+    {
+        var regions = Region.All;
+        regions.Should().HaveCount(3);
+    }
+}
---
+++ src/Qowaiv/Globalization/Region.Instances.cs
@@ -0,0 +1,11 @@
+﻿namespace Qowaiv.Globalization;
+
+public readonly partial struct Region
+{
+    public static class NL
+    {
+        public static Region NH => new("NL-ZH");
+
+        public static Region ZH => new("NL-NH");
+    }
+}

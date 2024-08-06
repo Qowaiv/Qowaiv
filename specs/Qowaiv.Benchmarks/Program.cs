f5ff4c80b70757df158f542fe52976f874ba14cf
Wesley Baartman <w.baartman@tjip.com>
2024-08-06 08:19:16 +0200

    Cache results of ToCSharpString

--- specs/Qowaiv.Benchmarks/Program.cs
+++ specs/Qowaiv.Benchmarks/Program.cs
@@ -6,6 +6,7 @@ public static class Program
 {
     public static void Main()
     {
+        BenchmarkRunner.Run<ToCSharpStringBenchmark>();
         BenchmarkRunner.Run<DecimalBenchmark>();
 
         BenchmarkRunner.Run<EmailBenchmark>();

e8bfdb4549fdd1448874bc81040de7bfee727557
Jack Kester <j.kester@tjip.com>
2024-10-10 11:50:06 +0200

    Add examples to the intellisense of Percentage.Create(), .Percent() and casts from and to numbers.
    Add examples to commonly used method in order to avoid confusion.

--- src/Qowaiv/Extensions/Humanizer.Percentage.cs
+++ src/Qowaiv/Extensions/Humanizer.Percentage.cs
@@ -5,15 +5,29 @@ namespace Qowaiv;
 /// <summary>Extensions to create <see cref="Percentage" />s, inspired by Humanizer.NET.</summary>
 public static class NumberToPercentageExtensions
 {
-    /// <summary>Interprets the <see cref="int" /> if it was written with a '%' sign.</summary>
+    /// <summary>Interprets the <see cref="int" /> if it was written with a '%' sign.
+    /// <example>E.g:
+    /// 12.Percent() => 12%
+    /// </example>
+    /// </summary>
     [Pure]
     public static Percentage Percent(this int number) => ((decimal)number).Percent();
 
-    /// <summary>Interprets the <see cref="double" /> if it was written with a '%' sign.</summary>
+    /// <summary>Interprets the <see cref="double" /> if it was written with a '%' sign.
+    /// <example>E.g:
+    /// 12.0.Percent() => 12%
+    /// 0.1.Percent() => 0.1%
+    /// </example>
+    /// </summary>
     [Pure]
     public static Percentage Percent(this double number) => Cast.ToDecimal<Percentage>(number).Percent();
 
-    /// <summary>Interprets the <see cref="decimal" /> if it was written with a '%' sign.</summary>
+    /// <summary>Interprets the <see cref="decimal" /> if it was written with a '%' sign.
+    /// <example>E.g:
+    /// 12m.Percent() => 12%
+    /// 0.1m.Percent() => 0.1%
+    /// </example>
+    /// </summary>
     [Pure]
     public static Percentage Percent(this decimal number) => Percentage.Create(DecimalMath.ChangeScale(number, -2));
 }

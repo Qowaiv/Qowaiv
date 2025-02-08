158cad4b79a140cd62aa542491e307741a0301ed
Wesley Baartman <9980136+CptWesley@users.noreply.github.com>
2025-02-08 18:24:48 +0100

    Simplify lock alias (#456)

--- src/Qowaiv/Globalization/LocalizedValues.cs
+++ src/Qowaiv/Globalization/LocalizedValues.cs
@@ -47,9 +47,5 @@ internal abstract class LocalizedValues<TValue> : Dictionary<CultureInfo, Dictio
     }
 
     /// <summary>The locker for adding a culture.</summary>
-#if NET9_0_OR_GREATER
     private readonly Lock locker = new();
-#else
-    private readonly object locker = new();
-#endif
 }

f90dd943e29b36e0c44433c3478540961f6ceffa
Corniel Nobel <corniel@gmail.com>
2025-03-07 11:29:10 +0100

    Fix

--- specs/Qowaiv.Specs/TestTools/Resx/XResourceFileData.cs
+++ specs/Qowaiv.Specs/TestTools/Resx/XResourceFileData.cs
@@ -43,40 +43,7 @@ public sealed class XResourceFileData : IComparable<XResourceFileData>
         : $"Data, Name: {Name}, Value: '{Value}'";
 
     public int CompareTo(XResourceFileData? other)
-        => other is null 
+        => other is null
         ? +1
-        : Comparer.Compare(Name, other.Name);
-
-    private static readonly FilePathComparer Comparer = new();
-
-    private sealed class FilePathComparer : IComparer<string?>
-    {
-        /// <inheritdoc />
-        public int Compare(string? x, string? y)
-        {
-            x ??= string.Empty;
-            y ??= string.Empty;
-            int? compare = null;
-            var xs = x.GetEnumerator();
-            var ys = y.GetEnumerator();
-
-            while (compare is null && xs.MoveNext() && ys.MoveNext())
-            {
-                compare = Compare(xs.Current, ys.Current);
-            }
-            return compare ?? x.Length.CompareTo(y.Length);
-        }
-
-        private static int? Compare(char x, char y)
-        {
-            int compare = Update(x).CompareTo(Update(y));
-            return compare == 0 ? null : compare;
-
-            static char Update(char ch)
-                => ch == '/' || ch == '\\'
-                ? char.MinValue
-                : char.ToUpperInvariant(ch);
-        }
-    }
-
+        : Name.CompareTo(other.Name, StringComparison.OrdinalIgnoreCase);
 }

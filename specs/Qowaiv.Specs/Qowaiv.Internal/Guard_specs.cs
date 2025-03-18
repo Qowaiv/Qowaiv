638ecc02c2c40023886fa79533a39af7b8a1334b
Corniel Nobel <corniel@gmail.com>
2025-03-18 20:09:29 +0100

    Hide Guard methods from stacktrace

---
+++ specs/Qowaiv.Specs/Qowaiv.Internal/Guard_specs.cs
@@ -0,0 +1,31 @@
+#if NET6_0_OR_GREATER
+
+namespace Guard_specs;
+
+public class Stacktrace
+{
+    [Test]
+    public void does_not_contain_guard_method_in_exception()
+    {
+        try
+        {
+            _ = Clock.Today(null!);
+        }
+        catch (ArgumentNullException x)
+        {
+            var trace = new StackTrace(x);
+
+            var method = trace.GetFrames()[0].GetMethod();
+
+            method.Should().BeEquivalentTo(
+                new 
+                {
+                    Name = "NotNull",
+                    DeclaringType = new { Name = "Guard" },
+                });
+
+            x.StackTrace.Should().NotContain("NotNull");
+        }
+    }
+}
+#endif

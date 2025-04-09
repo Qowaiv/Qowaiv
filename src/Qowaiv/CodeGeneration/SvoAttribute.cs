fdd44ee5f18ef85262cfe13acc2e55c760d909c1
Corniel Nobel <corniel@gmail.com>
2025-04-09 21:11:20 +0200

    SvoAttribute

---
+++ src/Qowaiv/CodeGeneration/SvoAttribute.cs
@@ -0,0 +1,21 @@
+using Qowaiv.Customization;
+
+namespace Qowaiv.CodeGeneration;
+
+/// <summary>
+/// Used by Qowaiv.CodeGeneration.SingleValueObjects to generateod the
+/// blumbping code for the custom SVO.
+/// </summary>
+/// <typeparam name="TBehavior">
+/// Singleton that handles the behavior of the custom SVO.
+/// </typeparam>
+[Conditional("CONTRACTS_FULL")]
+[AttributeUsage(AttributeTargets.Struct, AllowMultiple = false)]
+[ExcludeFromCodeCoverage]
+public sealed class SvoAttribute<TBehavior> : Attribute
+    where TBehavior : SvoBehavior, new()
+{
+    /// <inheritdoc />
+    [Pure]
+    public override string ToString() => $"[Svo<{typeof(TBehavior)}>]";
+}

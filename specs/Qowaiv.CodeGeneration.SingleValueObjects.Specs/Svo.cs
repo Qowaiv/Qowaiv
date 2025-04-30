e26d811c6b831ac1faf18f5410ee22f151a84651
Corniel Nobel <corniel@gmail.com>
2025-04-30 15:32:53 +0200

    Add SVO test

---
+++ specs/Qowaiv.CodeGeneration.SingleValueObjects.Specs/Svo.cs
@@ -0,0 +1,32 @@
+using Qowaiv.Customization;
+using Qowaiv.OpenApi;
+using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Specs;
+
+public static class Svo
+{
+    public static readonly CustomSvo CustomSvo = CustomSvo.Parse("QOWAIV");
+}
+
+[OpenApiDataType(description: "Custom SVO Example", type: "string", example: "QOWAIV", format: "custom")]
+[Svo<Behavior>]
+public readonly partial struct CustomSvo
+{
+    public static CustomSvo Parse(string s) => new();
+
+    private sealed class Behavior : SvoBehavior
+    {
+        public override int MinLength => 3;
+        public override int MaxLength => 16;
+        public override Regex Pattern => new("^[A-Z]+$", RegexOptions.Compiled, TimeSpan.FromMilliseconds(1));
+
+        public override string NormalizeInput(string? str, IFormatProvider? formatProvider)
+            => str?.Replace("-", "").ToUpper(formatProvider ?? CultureInfo.InvariantCulture) ?? string.Empty;
+
+        public override string InvalidFormatMessage(string? str, IFormatProvider? formatProvider)
+            => "Is not a valid CustomSvo";
+    }
+}

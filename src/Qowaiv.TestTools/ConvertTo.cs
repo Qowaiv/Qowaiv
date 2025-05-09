9d6bfdad6cc89141dd89dbbf659f5a1bce9afa82
Corniel Nobel <corniel@gmail.com>
2025-05-09 09:57:57 +0200

    Add Converting.From(Type).To<To>()

--- src/Qowaiv.TestTools/ConvertTo.cs
+++ src/Qowaiv.TestTools/ConvertTo.cs
@@ -1,14 +1,19 @@
 namespace Qowaiv.TestTools;
 
-/// <summary>Type converter builder to apply <see cref="TypeConverter.ConvertTo(object, System.Type)" />.</summary>
-public sealed class ConvertTo<To>
+/// <summary>Type converter builder to apply <see cref="TypeConverter.ConvertTo(object, Type)" />.</summary>
+public class ConvertTo
 {
-    internal ConvertTo() { }
+    /// <summary>The type to convert to.</summary>
+    protected Type To { get; }
+
+    /// <summary>Initializes a new instance of the <see cref="ConvertTo" /> class.</summary>
+    internal ConvertTo(Type to) => To = Guard.NotNull(to);
 
     /// <summary>Converts the value to the destination type, using the <see cref="TypeConverter" /> of the subject.</summary>
     [Pure]
-    public To? From<TFrom>(TFrom subject) => (To?)Converter<TFrom>().ConvertTo(subject, typeof(To));
+    public bool From<TFrom>() => Converter<TFrom>().CanConvertTo(To);
 
+    /// <summary>Creates a type converter for <typeparamref name="TFrom"/>.</summary>
     [Pure]
-    private static TypeConverter Converter<TFrom>() => TypeDescriptor.GetConverter(typeof(TFrom));
+    protected static TypeConverter Converter<TFrom>() => TypeDescriptor.GetConverter(typeof(TFrom));
 }

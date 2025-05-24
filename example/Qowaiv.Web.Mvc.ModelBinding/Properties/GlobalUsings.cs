0d7797e2d792997b335a0056d93b323b7e7197df
Corniel Nobel <corniel@gmail.com>
2025-05-24 11:29:57 +0200

    Drop MVC Model Binding example code (#495)

--- example/Qowaiv.Web.Mvc.ModelBinding/Properties/GlobalUsings.cs
+++
@@ -1,8 +0,0 @@
-global using System;
-global using System.Collections;
-global using System.Collections.Generic;
-global using System.ComponentModel;
-global using System.Diagnostics;
-global using System.Diagnostics.CodeAnalysis;
-global using System.Globalization;
-global using System.Linq;

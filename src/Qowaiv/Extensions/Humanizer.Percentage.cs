934483cdac49d8f65d8bd7f2a62eb6dfc1b008be
Jack Kester <j.kester@tjip.com>
2025-12-02 16:13:01 +0100

    Fix typo in Percentage intellisense

--- src/Qowaiv/Extensions/Humanizer.Percentage.cs
+++ src/Qowaiv/Extensions/Humanizer.Percentage.cs
@@ -5,7 +5,7 @@ namespace Qowaiv;
 /// <summary>Extensions to create <see cref="Percentage" />s, inspired by Humanizer.NET.</summary>
 public static class NumberToPercentageExtensions
 {
-    /// <summary>Interprets the <see cref="int" /> if it was written with a '%' sign.
+    /// <summary>Interprets the <see cref="int" /> as if it was written with a '%' sign.
     /// <example>E.g:
     /// 12.Percent() => 12%
     /// </example>
@@ -13,7 +13,7 @@ public static class NumberToPercentageExtensions
     [Pure]
     public static Percentage Percent(this int number) => ((decimal)number).Percent();
 
-    /// <summary>Interprets the <see cref="double" /> if it was written with a '%' sign.
+    /// <summary>Interprets the <see cref="double" /> as if it was written with a '%' sign.
     /// <example>E.g:
     /// 12.0.Percent() => 12%
     /// 0.1.Percent() => 0.1%
@@ -22,7 +22,7 @@ public static class NumberToPercentageExtensions
     [Pure]
     public static Percentage Percent(this double number) => Cast.ToDecimal<Percentage>(number).Percent();
 
-    /// <summary>Interprets the <see cref="decimal" /> if it was written with a '%' sign.
+    /// <summary>Interprets the <see cref="decimal" /> as if it was written with a '%' sign.
     /// <example>E.g:
     /// 12m.Percent() => 12%
     /// 0.1m.Percent() => 0.1%

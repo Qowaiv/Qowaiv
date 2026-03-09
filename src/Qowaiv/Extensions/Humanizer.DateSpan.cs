23670ad455471dd990ecd2a353372223f6a9f61f
Corniel Nobel <corniel@gmail.com>
2026-03-09 11:42:07 +0100

    Apply suggestions from code review
    Co-authored-by: Laura Kramer <laurakramer91@gmail.com>

--- src/Qowaiv/Extensions/Humanizer.DateSpan.cs
+++ src/Qowaiv/Extensions/Humanizer.DateSpan.cs
@@ -3,7 +3,7 @@ namespace Qowaiv;
 /// <summary>Extensions to create <see cref="DateSpan" />s, inspired by Humanizer.NET.</summary>
 public static class NumberToDateSpanExtensions
 {
-    /// <summary>Create a <see cref="YearSpan" /> from a <see cref="int" />.</summary>
+    /// <summary>Create a <see cref="DateSpan" /> from a <see cref="int" />.</summary>
     [Pure]
     public static DateSpan Days(this int days) => new(months: 0, days);
 }
--- src/Qowaiv/Extensions/Humanizer.MonthSpan.cs
+++ src/Qowaiv/Extensions/Humanizer.MonthSpan.cs
@@ -3,7 +3,7 @@ namespace Qowaiv;
 /// <summary>Extensions to create <see cref="MonthSpan" />s, inspired by Humanizer.NET.</summary>
 public static class NumberToMonthSpanExtensions
 {
-    /// <summary>Create a <see cref="YearSpan" /> from a <see cref="int" />.</summary>
+    /// <summary>Create a <see cref="MonthSpan" /> from a <see cref="int" />.</summary>
     [Pure]
     public static MonthSpan Months(this int months) => MonthSpan.FromMonths(months);
 }

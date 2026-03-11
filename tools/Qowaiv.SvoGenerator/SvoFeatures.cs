9a99536056e730a656e09ec2da687215eda57527
Corniel Nobel <corniel@gmail.com>
2026-03-11 22:19:23 +0100

    Update Flags

--- tools/Qowaiv.SvoGenerator/SvoFeatures.cs
+++ tools/Qowaiv.SvoGenerator/SvoFeatures.cs
@@ -1,5 +1,6 @@
 namespace Qowaiv.SvoGenerator;
 
+[Flags]
 public enum SvoFeatures
 {
     Field = /*...............*/ 0x00001,
@@ -8,20 +9,31 @@ public enum SvoFeatures
     IsUnknown = /*...........*/ 0x00008,
     IEquatable = /*..........*/ 0x00010,
     EqualsSvo = /*...........*/ 0x00020,
-    ISerializable = /*.......*/ 0x00080,
-    IJsonSerializable = /*...*/ 0x00100,
-    IFormattable = /*........*/ 0x00200,
-    IComparable = /*.........*/ 0x00400,
-    ComparisonOperators = /*.*/ 0x00800,
-    Parsing = /*.............*/ 0x01000,
-    Validation = /*..........*/ 0x02000,
-    All = /*.................*/ 0x03FFF,
+    ISerializable = /*.......*/ 0x00040,
+    IJsonSerializable = /*...*/ 0x00080,
+    IFormattable = /*........*/ 0x00100,
+    IComparable = /*.........*/ 0x00200,
+    ComparisonOperators = /*.*/ 0x00400,
+    Parsing = /*.............*/ 0x00800,
+    Validation = /*..........*/ 0x01000,
+    Utf8 = /*................*/ 0x02000,
+    All = Field
+        | GetHashCode
+        | IsEmpty
+        | IsUnknown
+        | IEquatable
+        | EqualsSvo
+        | ISerializable
+        | IJsonSerializable
+        | IFormattable
+        | IComparable
+        | ComparisonOperators
+        | Parsing
+        | Validation,
 
     Structure = Field | IsEmpty | IsUnknown,
     EqualsSvoAndGetHashCode = EqualsSvo | GetHashCode,
     IsEmptyOrUnknown = IsEmpty | IsUnknown,
     Default = All ^ ComparisonOperators ^ Validation,
     Continuous = All ^ IsEmpty ^ IsUnknown ^ Validation,
-
-    Utf8 = /*................*/ 0x04000,
 }

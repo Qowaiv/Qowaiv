a8dbeb02004593f255880d82bb4c2aeccf77d2a0
Corniel Nobel <corniel@gmail.com>
2026-03-25 09:54:47 +0100

    Add IBAN

--- src/Qowaiv.EntityFrameworkCore/SvoPropertyBuilder.cs
+++ src/Qowaiv.EntityFrameworkCore/SvoPropertyBuilder.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Qowaiv.Financial;
 using System.Linq.Expressions;
 
 namespace Qowaiv.EntityFrameworkCore;
@@ -9,9 +10,16 @@ public static class SvoPropertyBuilder
     extension<TEntity>(EntityTypeBuilder<TEntity> builder) where TEntity : class
     {
         /// <summary>
-        /// Returns a builder that can be used to configure an SVO property of the entity type.
-        /// If the specified property is not already part of the model, it will be added.
+        /// Returns a builder that can be used to configure an <see cref="EmailAddress"/>
+        /// SVO property of the entity type. If the specified property is not
+        /// already part of the model, it will be added.
         /// </summary>
+        /// <remarks>
+        /// Configured are:
+        /// * Conversion
+        /// * MaxLength
+        /// * Unicode.
+        /// </remarks>
         /// <param name="propertyExpression">
         /// A lambda expression representing the property to be configured (
         /// <c>model => model.Email</c>).
@@ -21,7 +29,30 @@ public static class SvoPropertyBuilder
         public PropertyBuilder<EmailAddress> SvoProperty(Expression<Func<TEntity, EmailAddress>> propertyExpression) => builder
             .Property(propertyExpression)
             .HasConversion(svo => svo.ToString(), str => EmailAddress.Parse(str))
-            .IsUnicode()
-            .HasMaxLength(EmailAddress.MaxLength);
+            .HasMaxLength(EmailAddress.MaxLength)
+            .IsUnicode();
+
+        /// <summary>
+        /// Returns a builder that can be used to configure an <see cref="InternationalBankAccountNumber"/>
+        /// SVO property of the entity type. If the specified property is not
+        /// already part of the model, it will be added.
+        /// </summary>
+        /// <remarks>
+        /// Configured are:
+        /// * Conversion
+        /// * MaxLength
+        /// * ASCII.
+        /// </remarks>
+        /// <param name="propertyExpression">
+        /// A lambda expression representing the property to be configured (
+        /// <c>model => model.Iban</c>).
+        /// </param>
+        [FluentSyntax]
+        [CLSCompliant(false)]
+        public PropertyBuilder<InternationalBankAccountNumber> SvoProperty(Expression<Func<TEntity, InternationalBankAccountNumber>> propertyExpression) => builder
+            .Property(propertyExpression)
+            .HasConversion(svo => svo.ToString(), str => InternationalBankAccountNumber.Parse(str))
+            .IsUnicode(false)
+            .HasMaxLength(34 /* InternationalBankAccountNumber.MaxLength */ );
     }
 }

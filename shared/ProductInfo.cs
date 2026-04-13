10b78b9d8d05232d04f1c068bb9d1e4d172892c8
Corniel Nobel <corniel@gmail.com>
2026-04-13 11:57:02 +0200

    Revert product info

--- shared/ProductInfo.cs
+++ shared/ProductInfo.cs
@@ -1,8 +1,8 @@
 // If not specified otherwise, the resources are in USA English.
-[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: System.Resources.NeutralResourcesLanguage("en-US")]
 
 // For optimal .NET support this is enabled in all assemblies.
-[assembly: CLSCompliant(true)]
+[assembly: System.CLSCompliant(true)]
 
 // No COM visibility.
 // Classes which are exposed to COM must have a parameterless constructor. That

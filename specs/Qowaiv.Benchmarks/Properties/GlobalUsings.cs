7dda3f36bf78964f3c08219ee1f11a6553051539
Corniel Nobel <corniel@gmail.com>
2026-05-05 12:44:06 +0200

    Packages

--- specs/Qowaiv.Benchmarks/Properties/GlobalUsings.cs
+++ specs/Qowaiv.Benchmarks/Properties/GlobalUsings.cs
@@ -7,4 +7,3 @@ global using System.Diagnostics.CodeAnalysis;
 global using System.Diagnostics.Contracts;
 global using System.Linq;
 global using System.Text;
-global using System.Text.RegularExpressions;

31df7497d01cdeaf704ae1801d3685d22eff1f7d
Corniel Nobel <corniel@gmail.com>
2026-07-14 13:05:14 +0200

    Split files

---
+++ src/Qowaiv/Extensions/System.Collections.Generic.Dictionary.cs
@@ -0,0 +1,13 @@
+#if !NET8_0_OR_GREATER
+namespace System.Collections.Generic;
+
+internal static class QowaivDictionaryExtensions
+{
+    extension<TKey, TValue>(IDictionary<TKey, TValue> dictionary) where TKey : notnull
+    {
+        [Pure]
+        public FrozenDictionary<TKey, TValue> ToFrozenDictionar()
+            => new(dictionary);
+    }
+}
+#endif

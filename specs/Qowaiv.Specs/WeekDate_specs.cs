9b71fa608511f9120e96f8ad168d7541a9a4ff6a
Corniel Nobel <corniel@gmail.com>
2026-09-24 13:52:44 +0200

    Define used SDK via global json (#632)

--- specs/Qowaiv.Specs/WeekDate_specs.cs
+++ specs/Qowaiv.Specs/WeekDate_specs.cs
@@ -512,7 +512,7 @@ public class Casts
     [Test]
     public void explicitly_from_DateTime()
     {
-        var casted = (WeekDate)new DateTime(2017, 06, 11, 0, 0, 0, DateTimeKind.Local);
+        var casted = (WeekDate)new DateTime(2017, 06, 11, 00, 00, 00, DateTimeKind.Local);
         casted.Should().Be(Svo.WeekDate);
     }
 
@@ -520,7 +520,7 @@ public class Casts
     public void implicitly_to_DateTime()
     {
         DateTime casted = Svo.WeekDate;
-        casted.Should().Be(new DateTime(2017, 06, 11, 0, 0, 0, DateTimeKind.Local));
+        casted.Should().Be(new DateTime(2017, 06, 11, 00, 00, 00, DateTimeKind.Local));
     }
 }
 #endif
